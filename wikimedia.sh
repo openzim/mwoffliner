462c229ca8c30661d37ee917db09ac0686ea5d90
kelson42 <kelson42@user.sourceforge.net>
2014-11-17 23:34:28 +0100

    + wikibooks

--- wikimedia.sh
+++ wikimedia.sh
@@ -3,3 +3,4 @@
 ./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikinews    --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikinews/
 ./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiquote   --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikiquote/
 ./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiversity --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikiversity/
+./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikibooks   --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikibooks/
