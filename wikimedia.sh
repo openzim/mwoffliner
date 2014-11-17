c7a410dfc7af502791a7ce1cf8f97bddec531dc5
kelson42 <kelson42@user.sourceforge.net>
2014-11-17 13:28:14 +0100

    + wikiquote

--- wikimedia.sh
+++ wikimedia.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikivoyage --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikivoyage/
 ./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikinews   --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikinews/
+./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiquote  --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikiquote/
