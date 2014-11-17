60a4170c34925f8573b368dc752c14112981a39a
kelson42 <kelson42@user.sourceforge.net>
2014-11-17 19:52:21 +0100

    + Wikiversity

--- wikimedia.sh
+++ wikimedia.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikivoyage --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikivoyage/
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikinews   --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikinews/
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiquote  --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikiquote/
+./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikivoyage  --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikivoyage/
+./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikinews    --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikinews/
+./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiquote   --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikiquote/
+./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiversity --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikiversity/
