59f2bfc47f689ac20f7c914223bbc6e66f3d64e6
kelson42 <kelson42@user.sourceforge.net>
2014-11-27 12:01:12 +0100

    + wikispecies

--- wikimedia.sh
+++ wikimedia.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikivoyage  --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikivoyage/
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikinews    --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikinews/
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiquote   --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikiquote/
+
+# Wikivoyage
+./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikivoyage --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikivoyage/
+
+# Wikinews
+./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikinews --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikinews/
+
+# Wikiquote
+./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiquote --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikiquote/
+
+# Wikiversity
 ./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiversity --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikiversity/
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikibooks   --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikibooks/
+
+# Wikibooks
+./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikibooks --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikibooks/
+
+# Wikispecies
+./mwoffliner.js --mwUrl=http://species.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/species/ --format= --format=nopic --outputDirectory=species --filenamePrefix="wikispecies_en_all"
+mv species/*.zim /var/www/zimfarm.kiwix.org/upload/zim2index/wikispecies
