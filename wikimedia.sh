8b5cb7cbcaad62e88fe4f1ca3e7defb6302a6b20
kelson42 <kelson42@user.sourceforge.net>
2015-01-11 19:20:23 +0100

    Small improvements

--- wikimedia.sh
+++ wikimedia.sh
@@ -1,20 +1,39 @@
 #!/bin/sh
 
+ZIM2INDEX=`readlink -f $1`
+SCRIPT=`readlink -f $0`
+SCRIPT_DIR=`dirname "$SCRIPT"`
+MWOFFLINER="$SCRIPT_DIR/mwoffliner.js"
+MWMATRIXOFFLINER="$SCRIPT_DIR/mwmatrixoffliner.js"
+
+# Few checks
+if [ $# -lt 1 ]
+then
+    echo "Not enough argument. This script need as first and only argument the full path of the 'zim2index' directory where the produced ZIM files need to be moved"
+else
+    if [ -d "$ZIM2INDEX" ]
+    then
+	echo "Target directory for ZIM file is '$ZIM2INDEX'"
+    else
+	echo "Unable to find or to write into directory '$ZIM2INDEX'"
+    fi
+fi
+
 # Wikivoyage
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikivoyage --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikivoyage/
+$MWMATRIXOFFLINER --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikivoyage --outputDirectory=$ZIM2INDEX/wikivoyage/
 
 # Wikinews
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikinews --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikinews/
+$MWMATRIXOFFLINER --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikinews --outputDirectory=$ZIM2INDEX/wikinews/
 
 # Wikiquote
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiquote --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikiquote/
+$MWMATRIXOFFLINER --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiquote --outputDirectory=$ZIM2INDEX/wikiquote/
 
 # Wikiversity
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiversity --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikiversity/
+$MWMATRIXOFFLINER --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikiversity --outputDirectory=$ZIM2INDEX/wikiversity/
 
 # Wikibooks
-./mwmatrixoffliner.js --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikibooks --outputDirectory=/var/www/zimfarm.kiwix.org/upload/zim2index/wikibooks/
+$MWMATRIXOFFLINER --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikibooks --outputDirectory=$ZIM2INDEX/wikibooks/
 
 # Wikispecies
-./mwoffliner.js --mwUrl=http://species.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/species/ --format= --format=nopic --outputDirectory=species --filenamePrefix="wikispecies_en_all"
-mv species/*.zim /var/www/zimfarm.kiwix.org/upload/zim2index/wikispecies
+$MWOFFLINER --mwUrl=http://species.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/species/ --format= --format=nopic --outputDirectory=species --filenamePrefix="wikispecies_en_all"
+mv species/*.zim $ZIM2INDEX/wikispecies
