91ac3b26dca41d1c694892f24be4c5f81fad24ae
kelson42 <kelson42@user.sourceforge.net>
2015-05-01 11:22:21 +0200

    Fix dumping scripts

--- servers/mwoffliner2_shm.sh
+++ servers/mwoffliner2_shm.sh
@@ -4,7 +4,7 @@ ZIM2INDEX=`readlink -f $1 2> /dev/null`
 SCRIPT=`readlink -f $0/../`
 SCRIPT_DIR=`dirname "$SCRIPT"`
 MWOFFLINER="$SCRIPT_DIR/mwoffliner.js"
-MWMATRIXOFFLINER="$SCRIPT_DIR/mwmatrixoffliner.js --verbose --speed=3 --skipHtmlCache --adminEmail=contact@kiwix.org --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --cacheDirectory=/data/project/cac/ --tmpDirectory=/dev/shm/"
+MWMATRIXOFFLINER="$SCRIPT_DIR/mwmatrixoffliner.js --verbose --speed=3 --skipHtmlCache --adminEmail=contact@kiwix.org --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://rest.wikimedia.org/ --cacheDirectory=/data/project/cac/ --tmpDirectory=/dev/shm/"
 
 # Few checks
 if [ $# -lt 1 ]
