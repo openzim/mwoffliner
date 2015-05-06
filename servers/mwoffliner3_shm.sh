51348651f07a2dcfd5b3e3eda5db392330371099
kelson42 <kelson42@user.sourceforge.net>
2015-05-06 10:28:50 +0200

    Small fix

--- servers/mwoffliner3_shm.sh
+++ servers/mwoffliner3_shm.sh
@@ -37,4 +37,4 @@ $MWMATRIXOFFLINER --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=
 $MWMATRIXOFFLINER --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=wikibooks --outputDirectory=$ZIM2INDEX/wikibooks/ &&
 
 # Wikipedia
-$MWMATRIXOFFLINER --parsoidUrl=http://rest.wikimedia.org/ --deflateTmpHtml --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --languageInverter --language="(es|de|ru|ja|fr|zh|it|ar|bg|cs|da|et|el|eo|eu|gl|hy|hi|hr|ms|min|nn|uz|kk|ro|simple|sk|sl|sr|sh|fi|tr|vo|pl|pt|nl|ceb|sv|vi|war|fa|ca|ko|id|he|la|lt|hu|no|uk)"
+$MWMATRIXOFFLINER --parsoidUrl=http://rest.wikimedia.org/ --deflateTmpHtml --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --languageInverter --language="(en|es|de|ru|ja|fr|zh|it|ar|bg|cs|da|et|el|eo|eu|gl|hy|hi|hr|ms|min|nn|uz|kk|ro|simple|sk|sl|sr|sh|fi|tr|vo|pl|pt|nl|ceb|sv|vi|war|fa|ca|ko|id|he|la|lt|hu|no|uk)"
