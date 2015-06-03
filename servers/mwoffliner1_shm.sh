8fc6bd2448623057084ebfa42821ef28d7cf9855
kelson42 <kelson42@user.sourceforge.net>
2015-06-03 21:44:02 +0200

    Fix ZIM2INDEX directory

--- servers/mwoffliner1_shm.sh
+++ servers/mwoffliner1_shm.sh
@@ -1,25 +1,10 @@
 #!/bin/sh
 
-ZIM2INDEX=`readlink -f $1 2> /dev/null`
+ZIM2INDEX=/srv/upload/zim2index/
 SCRIPT=`readlink -f $0/../`
 SCRIPT_DIR=`dirname "$SCRIPT"`
 MWOFFLINER="$SCRIPT_DIR/mwoffliner.js"
 MWMATRIXOFFLINER="$SCRIPT_DIR/mwmatrixoffliner.js --speed=3 --verbose --skipHtmlCache --adminEmail=contact@kiwix.org --mwUrl=http://meta.wikimedia.org/ --cacheDirectory=/data/project/cac/ --tmpDirectory=/dev/shm/ --skipHtmlCacheCleaning --skipCacheCleaning"
-
-# Few checks
-if [ $# -lt 1 ]
-then
-    echo "Not enough argument. This script need as first and only argument the full path of the 'zim2index' directory where the produced ZIM files need to be moved"
-    exit 1;
-else
-    if [ -d "$ZIM2INDEX" ]
-    then
-	echo "Target directory for ZIM file is '$ZIM2INDEX'"
-    else
-	echo "Unable to find or to write into directory '$ZIM2INDEX'"
-	exit 1;
-    fi
-fi
 
 # Wikispecies
 $MWMATRIXOFFLINER --parsoidUrl=http://parsoid-lb.eqiad.wikimedia.org/ --project=species --outputDirectory=$ZIM2INDEX/wikispecies/ &&
