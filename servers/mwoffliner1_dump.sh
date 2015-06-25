46f32185cfbcaf29dbe50098b58b69a09121446a
kelson42 <kelson42@user.sourceforge.net>
2015-06-25 09:28:24 +0200

    Simplification

--- servers/mwoffliner1_dump.sh
+++ servers/mwoffliner1_dump.sh
@@ -6,5 +6,8 @@ SCRIPT_DIR=`dirname "$SCRIPT"`
 MWOFFLINER="$SCRIPT_DIR/mwoffliner.js"
 MWMATRIXOFFLINER="$SCRIPT_DIR/mwmatrixoffliner.js --speed=3 --verbose --skipHtmlCache --adminEmail=contact@kiwix.org --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://rest.wikimedia.org/ --cacheDirectory=/data/scratch/mwoffliner/ --deflateTmpHtml --skipCacheCleaning"
 
+# Wikipedia
+$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --language="(es|de|ru|ja|war)"
+
 # Wikisource
 $MWMATRIXOFFLINER --project=wikisource --outputDirectory=$ZIM2INDEX/wikisource/ --language=fr
