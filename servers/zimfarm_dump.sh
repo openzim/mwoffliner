b837dbaa3503d301ba09ca06cffbc589e16e1eda
kelson42 <kelson42@user.sourceforge.net>
2015-08-01 19:01:32 +0200

    Increase speed

--- servers/zimfarm_dump.sh
+++ servers/zimfarm_dump.sh
@@ -3,7 +3,7 @@
 ZIM2INDEX=/srv/upload/zim2index/
 SCRIPT=`readlink -f $0/../`
 SCRIPT_DIR=`dirname "$SCRIPT"`
-MWOFFLINER="$SCRIPT_DIR/mwoffliner.js --speed=3 --verbose --skipHtmlCache --skipCacheCleaning  --adminEmail=kelson@kiwix.org"
+MWOFFLINER="$SCRIPT_DIR/mwoffliner.js --speed=4 --verbose --skipHtmlCache --skipCacheCleaning  --adminEmail=kelson@kiwix.org"
 MWMATRIXOFFLINER="$SCRIPT_DIR/mwmatrixoffliner.js --speed=4 --verbose --skipHtmlCache --adminEmail=contact@kiwix.org --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://rest.wikimedia.org/ --deflateTmpHtml --skipCacheCleaning"
 
 # Wikipedia medicine
