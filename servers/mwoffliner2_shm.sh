0f991420b0719ebc83455acf6e55cd0fd6083931
kelson42 <kelson42@user.sourceforge.net>
2015-08-09 09:34:12 +0200

    Mv wpfa

--- servers/mwoffliner2_shm.sh
+++ servers/mwoffliner2_shm.sh
@@ -7,4 +7,4 @@ MWOFFLINER="$SCRIPT_DIR/mwoffliner.js"
 MWMATRIXOFFLINER="$SCRIPT_DIR/mwmatrixoffliner.js --verbose --speed=3 --skipHtmlCache --deflateTmpHtml --adminEmail=contact@kiwix.org --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://rest.wikimedia.org/ --cacheDirectory=/data/scratch/mwoffliner/cac/ --tmpDirectory=/dev/shm/ --skipCacheCleaning"
 
 # Wikipedia
-$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --language="(ar|bg|cs|da|et|el|eo|eu|gl|he|hy|hi|hr|id|fa|lt|la|ms|min|nn|no|uz|kk|ro|simple|sk|sl|sr|sh|fi|uk|tr|vo)"
+$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --language="(ar|bg|cs|da|et|el|eo|eu|fi|gl|he|hy|hi|hr|id|kk|lt|la|ms|min|nn|no|ro|simple|sk|sl|sr|sh|uk|uz|tr|vo)"
