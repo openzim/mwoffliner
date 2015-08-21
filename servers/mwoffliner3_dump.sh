f1cf55cc35fc00abdaa51fe2a60d325b9d5071fc
kelson42 <kelson42@user.sourceforge.net>
2015-08-21 09:02:05 +0200

    mv wpro

--- servers/mwoffliner3_dump.sh
+++ servers/mwoffliner3_dump.sh
@@ -7,4 +7,4 @@ MWOFFLINER="$SCRIPT_DIR/mwoffliner.js"
 MWMATRIXOFFLINER="$SCRIPT_DIR/mwmatrixoffliner.js --speed=3 --verbose --skipHtmlCache --adminEmail=contact@kiwix.org --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://rest.wikimedia.org/ --cacheDirectory=/data/scratch/mwoffliner/cac/ --deflateTmpHtml --skipCacheCleaning"
 
 # Wikipedia
-$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --language="(ceb|fa|it|zh)"
+$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --language="(ceb|fa|it|ro|zh)"
