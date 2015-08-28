b9b08ca5fa7b1c31d51505b94993b7509f48cca0
kelson42 <kelson42@user.sourceforge.net>
2015-08-28 07:54:19 +0200

    mv "wpvo"

--- servers/mwoffliner3_shm.sh
+++ servers/mwoffliner3_shm.sh
@@ -19,4 +19,4 @@ $MWMATRIXOFFLINER --project=wikiquote --outputDirectory=$ZIM2INDEX/wikiquote/ &&
 $MWMATRIXOFFLINER --project=wikiversity --outputDirectory=$ZIM2INDEX/wikiversity/ &&
 
 # Wikipedia
-$MWMATRIXOFFLINER --deflateTmpHtml --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --languageInverter --language="(en|es|de|ru|ja|fr|zh|it|ar|bg|cs|da|et|el|eo|eu|gl|hy|hi|hr|ms|min|nn|uz|kk|ro|simple|sk|sl|sr|sh|fi|tr|vo|pl|pt|nl|ceb|sv|vi|war|fa|ca|ko|id|he|la|lt|hu|no|uk)"
+$MWMATRIXOFFLINER --deflateTmpHtml --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --languageInverter --language="(ar|bg|ceb|cs|da|de|el|en|eo|eu|et|es|fi|fr|gl|hu|hy|hi|hr|it|ja|kk|ms|min|nl|nn|no|ro|simple|sk|sl|sr|sh|tr|pl|pt|ru|sv|vi|war|fa|ca|ko|id|he|la|lt|uk|uz|zh)"
