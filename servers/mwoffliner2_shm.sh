b2c913301c5d89a93c9bc406bc64e8a66820fc82
kelson42 <kelson42@user.sourceforge.net>
2015-08-28 07:44:46 +0200

    sort alph

--- servers/mwoffliner2_shm.sh
+++ servers/mwoffliner2_shm.sh
@@ -7,4 +7,4 @@ MWOFFLINER="$SCRIPT_DIR/mwoffliner.js"
 MWMATRIXOFFLINER="$SCRIPT_DIR/mwmatrixoffliner.js --verbose --speed=3 --skipHtmlCache --deflateTmpHtml --adminEmail=contact@kiwix.org --mwUrl=http://meta.wikimedia.org/ --parsoidUrl=http://rest.wikimedia.org/ --cacheDirectory=/data/scratch/mwoffliner/cac/ --tmpDirectory=/dev/shm/ --skipCacheCleaning"
 
 # Wikipedia
-$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --language="(ar|bg|cs|da|et|el|eo|eu|fi|gl|hy|hi|hr|id|kk|lt|la|ms|min|nn|no|simple|sk|sl|sr|uk|uz|tr|vo)"
+$MWMATRIXOFFLINER --project=wiki --outputDirectory=$ZIM2INDEX/wikipedia/ --language="(ar|bg|cs|da|et|el|eo|eu|fi|gl|hy|hi|hr|id|kk|lt|la|ms|min|nn|no|simple|sk|sl|sr|tr|uk|uz|vo)"
