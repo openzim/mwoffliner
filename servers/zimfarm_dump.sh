63241f378578bde416c8b614f2ffee29416f1bb1
kelson42 <kelson@kiwix.org>
2016-07-16 19:39:41 +0200

    More depth to detect Europe related Wikivoyages articles

--- servers/zimfarm_dump.sh
+++ servers/zimfarm_dump.sh
@@ -120,7 +120,7 @@ $MWOFFLINER --speed=5 --mwUrl="https://he.wikipedia.org/" --parsoidUrl="https://
 $MWOFFLINER --speed=5 --mwUrl="https://en.wikivoyage.org/" --parsoidUrl="https://en.wikivoyage.org/api/rest_v1/page/html/" --customMainPage="Wikivoyage:Offline_reader_Expedition/Home_page" --outputDirectory=$ZIM2INDEX/wikivoyage/ &&
 
 # Wikivoyage europe in english
-/srv/kiwix-tools/tools/scripts/listCategoryEntries.pl --host=en.wikivoyage.org --path=w --exploration=5 --namespace=0 --category="Europe" | sort -u > europe &&
+/srv/kiwix-tools/tools/scripts/listCategoryEntries.pl --host=en.wikivoyage.org --path=w --exploration=8 --namespace=0 --category="Europe" | sort -u > europe &&
 $MWOFFLINER --customZimTitle="Wikivoyage - Europe" --speed=5 --articleList=europe --mwUrl="https://en.wikivoyage.org/" --parsoidUrl="https://en.wikivoyage.org/api/rest_v1/page/html/" --customMainPage="Wikivoyage:Offline_reader_Expedition/Europe_Home_page" --outputDirectory=$ZIM2INDEX/wikivoyage/ &&
 
 # Wikipedia
