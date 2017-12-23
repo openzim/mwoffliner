openzim/mwoffliner

commit aa43ae1a443d52e251995f2a74709b6962490f60

Extract failure pattern into a helper method

lib/Utils.js
@@ -56,6 +56,13 @@ var Utils = {
 
       return url;
     },
+
+    exitIfError: function(error, msg) {
+      if (error) {
+        console.error(msg);
+        process.exit(1);
+      }
+    },
 };
 
 module.exports = {

commit dce7f3e297dca70a6c3bf34ec557bbaefd553f10

Finish up failure refactoring started in aa43ae1a

lib/MediaWiki.js
@@ -46,13 +46,9 @@ MediaWiki.prototype.login = function(downloader, cb) {
                     body = content.toString();
                     jsonResponse = JSON.parse(body).login;
 
-                    if (jsonResponse.result == 'Success') {
-                        downloader.loginCookie = jsonResponse.cookieprefix + '_session=' + jsonResponse.sessionid;
-                        cb();
-                    } else {
-                        console.error('Login failed');
-                        process.exit(1);
-                    }
+                    U.exitIfError(jsonResponse.result !== 'Success', 'Login Failed');
+                    downloader.loginCookie = jsonResponse.cookieprefix + '_session=' + jsonResponse.sessionid;
+                    cb();
                 });
             }
         });

lib/OfflinerEnv.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var urlParser = require('url');
 var pathParser = require('path');
+var U = require('./Utils.js').Utils;
 
 // This is just a refactoring stub for now.
 // Eventually, we want a MWOffliner object that might swallow this.
@@ -37,10 +38,8 @@ function OfflinerEnv(format, envObjs) {
             this.dumps = [format];
         }
 
-        if (this.dumps.indexOf('nodet') != -1 && !this.zim.mobileLayout) {
-            console.error('The "nodet" format can only work with --mobileLayout');
-            process.exit(1);
-        }
+        U.exitIfError(this.dumps.indexOf('nodet') !== -1 && !this.zim.mobileLayout,
+            'The "nodet" format can only work with --mobileLayout');
     }
 
     // Update the other config objects

lib/Utils.js
@@ -5,7 +5,7 @@ const urlParser = require('url');
 const pathParser = require('path');
 
 var Utils = {
-    validateEmail: function(email) {
+    isValidEmail: function(email) {
         var emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return emailRegex.test(email);
     },

lib/Zim.js
@@ -10,6 +10,7 @@ const pathParser = require('path');
 const fs = require('fs');
 const urlParser = require('url');
 const ci = require('case-insensitive');
+const U = require('./Utils.js').Utils;
 
 function Zim(config, args) {
     this.config = config;
@@ -31,12 +32,8 @@ Zim.prototype.createDirectories = function(cb) {
             function(finished) { mkdirp(self.tmpDirectory, finished); },
         ],
         function (error) {
-            if (error) {
-                console.error('Unable to create mandatory directories : ' + error);
-                process.exit(1);
-            } else {
-                cb();
-            }
+            U.exitIfError(error, `Unable to create mandatory directories : ${error}`);
+            cb();
         }
     );
 };
@@ -57,12 +54,8 @@ Zim.prototype.createSubDirectories = function(finished) {
       finished => fs.mkdir(env.htmlRootPath + dirs.javascript + '/' + dirs.jsModules, undefined, finished),
     ],
     error => {
-      if (error) {
-        console.error(`Unable to create mandatory directories : ${error}`);
-        process.exit(1);
-      } else {
-        finished();
-      }
+      U.exitIfError(error, `Unable to create mandatory directories : ${error}`);
+      finished();
     }
   );
 }
@@ -128,8 +121,7 @@ Zim.prototype.executeTransparently = function(command, args, callback, nostdout,
     var logger = this.env.logger;
     try {
         var proc = spawn(command, args).on('error', function (error) {
-            console.error('Error in executeTransparently(), ' + error);
-            process.exit(1);
+            U.exitIfError(error, 'Error in executeTransparently(), ' + error);
         });
 
         if (!nostdout) {
@@ -201,12 +193,8 @@ Zim.prototype.buildZIM = function(cb) {
                     zimPath
                 ],
                 function (error) {
-                    if (error) {
-                        console.error('Failed to build successfuly the ZIM file ' + zimPath + ' (' + error + ')');
-                        process.exit(1);
-                    } else {
-                        logger.log('ZIM file built at ' + zimPath);
-                    }
+                    U.exitIfError(error, 'Failed to build successfuly the ZIM file ' + zimPath + ' (' + error + ')');
+                    logger.log('ZIM file built at ' + zimPath);
 
                     /* Delete the html directory ? */
                     if (env.keepHtml) {

lib/mwoffliner.lib.js
@@ -45,10 +45,7 @@ function execute(argv) {
 
   /* HTTP user-agent string */
   const adminEmail = argv.adminEmail;
-  if (!U.validateEmail(adminEmail)) {
-    console.error('Admin email ' + adminEmail + ' is not valid');
-    process.exit(1);
-  }
+  U.exitIfError(!U.isValidEmail(adminEmail), 'Admin email ' + adminEmail + ' is not valid');
 
   /* Parsoid URL */
   let parsoidUrl = argv.parsoidUrl;
@@ -56,17 +53,11 @@ function execute(argv) {
 
   /* ZIM custom Favicon */
   const customZimFavicon = argv.customZimFavicon;
-  if (customZimFavicon && !fs.existsSync(customZimFavicon)) {
-    console.error('Path "' + customZimFavicon + '" is not a valid PNG file.');
-    process.exit(1);
-  }
+  U.exitIfError(customZimFavicon && !fs.existsSync(customZimFavicon), `Path ${customZimFavicon} is not a valid PNG file.`);
 
   /* Number of parallel requests */
+  U.exitIfError(argv.speed && isNaN(argv.speed), 'speed is not a number, please give a number value to --speed');
   const cpuCount = os.cpus().length;
-  if (argv.speed && isNaN(argv.speed)) {
-    console.error('speed is not a number, please give a number value to --speed');
-    process.exit(1);
-  }
   const speed = cpuCount * (argv.speed || 1);
 
   /* Necessary to avoid problems with https */
@@ -233,8 +224,7 @@ function execute(argv) {
           console.info('Parsoid Started Successfully');
         })
         .catch(err => {
-          console.error(`Error starting Parsoid: ${err}`);
-          process.exit(1);
+          U.exitIfError(true, `Error starting Parsoid: ${err}`);
         });
       parsoidUrl = `http://localhost:8000/${webUrlHost}/v3/page/pagebundle/`;
       parsoidContentType = 'json';
@@ -681,9 +671,7 @@ function execute(argv) {
       try {
         finished(null, domino.createDocument(html), articleId);
       } catch (error) {
-        console.error(`Crash while parsing ${articleId}`);
-        console.error(error.stack);
-        process.exit(1);
+        U.exitIfError(true, `Crash while parsing ${articleId}\n${error.stack}`);
       }
     }
 
@@ -1113,11 +1101,8 @@ function execute(argv) {
             }
 
             /* Check if the link is "valid" */
-            if (!href) {
-              console.error(`No href attribute in the following code, in article ${articleId}`);
-              console.error(linkNode.outerHTML);
-              process.exit(1);
-            }
+            U.exitIfError(!href,
+              `No href attribute in the following code, in article ${articleId}\n${linkNode.outerHTML}`);
 
             /* Rewrite external links starting with // */
             if (rel.substring(0, 10) === 'mw:ExtLink' || rel === 'nofollow') {
@@ -1826,9 +1811,7 @@ function execute(argv) {
               logger.log(`Unable to get revisions for ${entry.title}, but entry exists in the database. Article was probably deleted meanwhile.`);
               delete articleIds[entry.title];
             } else {
-              console.error(`Unable to get revisions for ${entry.title}`);
-              console.error(`JSON was ${body}`);
-              process.exit(1);
+              U.exitIfError(true, `Unable to get revisions for ${entry.title}\nJSON was ${body}`);
             }
           }
         });
@@ -1872,8 +1855,7 @@ function execute(argv) {
       try {
         lines = fs.readFileSync(zim.articleList).toString().split('\n');
       } catch (error) {
-        console.error(`Unable to open article list file: ${error}`);
-        process.exit(1);
+        U.exitIfError(true, `Unable to open article list file: ${error}`);
       }
 
       async.eachLimit(lines, speed, getArticleIdsForLine, function(error) {
@@ -2035,10 +2017,9 @@ function execute(argv) {
             } else {
               fs.symlink(cachePath, mediaPath, 'file', function(error) {
                 if (error) {
-                  if (error.code != 'EEXIST') {
-                    console.error(`Unable to create symlink to ${mediaPath} at ${cachePath}: ${error}`);
-                    process.exit(1);
-                  } else if (!skipCacheCleaning) {
+                  U.exitIfError(error.code != 'EEXIST',
+                    `Unable to create symlink to ${mediaPath} at ${cachePath}: ${error}`);
+                  if (!skipCacheCleaning) {
                     U.touch(cachePath);
                   }
                 }
@@ -2062,10 +2043,8 @@ function execute(argv) {
               } else {
                 logger.log(`Caching ${filenameBase} at ${cachePath}...`);
                 fs.symlink(cachePath, mediaPath, 'file', function(error) {
-                  if (error && error.code != 'EEXIST') {
-                    console.error(`Unable to create symlink to ${mediaPath} at ${cachePath}: ${error}`);
-                    process.exit(1);
-                  }
+                  U.exitIfError(error && error.code != 'EEXIST',
+                    `Unable to create symlink to ${mediaPath} at ${cachePath}: ${error}`);
                   fs.writeFile(cacheHeadersPath, JSON.stringify({ width: width }), function(error) {
                     U.exitIfError(error, `Unable to write cache header at ${cacheHeadersPath}: ${error}`);
                     callback();
@@ -2087,13 +2066,9 @@ function execute(argv) {
   function downloadFile(url, path, force, callback) {
     fs.stat(path, function(error) {
       if (error && !force) {
-        if (error.code === 'ENOENT') {
-          logger.log(`${path} already downloaded, download will be skipped.`);
-          callback();
-        } else {
-          logger.log(`Impossible to stat() ${path}: ${error}`);
-          process.exit(1);
-        }
+        U.exitIfError(error.code !== 'ENOENT', `Impossible to stat() ${path}: ${error}`);
+        logger.log(`${path} already downloaded, download will be skipped.`);
+        callback();
       } else {
         logger.log(`Downloading ${decodeURI(url)} at ${path}...`);
         downloader.downloadContent(url, function(content, responseHeaders) {

lib/redis.js
@@ -77,8 +77,7 @@ Redis.prototype.processRedirectIfExists = function(targetId, processor) {
       processor(res);
     });
   } catch (error) {
-    console.error(`Exception by requesting redis ${error}`);
-    process.exit(1);
+    U.exitIfError(true, `Exception by requesting redis ${error}`);
   }
 };
 