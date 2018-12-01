0876032031fa0c8c8a19b0b3566b34b50bf14117
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-01 16:53:28 +0100

    Improved logging

--- src/Logger.ts
+++ src/Logger.ts
@@ -3,10 +3,27 @@ class Logger {
   constructor(verbose) {
     this.verbose = verbose;
   }
-  public log(msg) {
+
+  public getTs(prefix: string) {
+    return `${prefix} [${(new Date()).toISOString()}]`;
+  }
+
+  public info(...args) {
     if (this.verbose) {
-      console.info(msg);
+      console.info(this.getTs('[info]'), ...args);
     }
+  }
+
+  public log(...args) {
+    console.log(this.getTs('[log]'), ...args);
+  }
+
+  public warn(...args) {
+    console.warn(this.getTs('[warn]'), ...args);
+  }
+
+  public error(...args) {
+    console.error(this.getTs('[error]'), ...args);
   }
 }
 
