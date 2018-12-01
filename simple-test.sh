1fd7ada013064d819451fc10e54432972390f9de
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-01 16:36:51 +0100

    Simplified test

--- simple-test.sh
+++ simple-test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --verbose --format=nozim --articleList=./articleList
+./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --format=nozim --articleList=./articleList
 mv tmp ../test-run-change
 
-rm -rf cac
 git checkout master
+
+rm -rf cac
+redis-cli flushall
+
 npm ci
 npm run build
-./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --verbose --format=nozim --articleList=./articleList
+./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --format=nozim --articleList=./articleList
 mv tmp ../test-run-master
 
 
