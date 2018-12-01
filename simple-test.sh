8a6f3ffd619518d45ad56d7c1be5b79055efa9bf
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-01 15:57:43 +0100

    Compare branch to master, rather than released

--- simple-test.sh
+++ simple-test.sh
@@ -3,8 +3,8 @@
 ./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --verbose --format=nozim --articleList=./articleList
 mv tmp ../test-run-change
 
-npm i -g mwoffliner
-mwoffliner --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --verbose --format=nozim --articleList=./articleList
+git checkout master
+./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --verbose --format=nozim --articleList=./articleList
 mv tmp ../test-run-master
 
 
