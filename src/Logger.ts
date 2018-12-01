26eb838050622e3c4c5c5f6152a04bc83c294c48
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-01 17:04:47 +0100

    Improved logger and made warn a verbose log

--- src/Logger.ts
+++ src/Logger.ts
@@ -4,26 +4,28 @@ class Logger {
     this.verbose = verbose;
   }
 
-  public getTs(prefix: string) {
-    return `${prefix} [${(new Date()).toISOString()}]`;
+  public getTs() {
+    return (new Date()).toISOString();
   }
 
   public info(...args) {
     if (this.verbose) {
-      console.info(this.getTs('[info]'), ...args);
+      console.info(`[info] [${this.getTs()}]`, ...args);
     }
   }
 
   public log(...args) {
-    console.log(this.getTs('[log]'), ...args);
+    console.log(`[log] [${this.getTs()}]`, ...args);
   }
 
   public warn(...args) {
-    console.warn(this.getTs('[warn]'), ...args);
+    if (this.verbose) {
+      console.warn(`[warn] [${this.getTs()}]`, ...args);
+    }
   }
 
   public error(...args) {
-    console.error(this.getTs('[error]'), ...args);
+    console.error(`[error] [${this.getTs()}]`, ...args);
   }
 }
 
