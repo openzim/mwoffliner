8bf6177b3948a88fbdb777519c3bb64a6c994742
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-01 16:19:43 +0100

    Added cache removal

--- simple-test.sh
+++ simple-test.sh
@@ -3,6 +3,7 @@
 ./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --verbose --format=nozim --articleList=./articleList
 mv tmp ../test-run-change
 
+rm -rf cac
 git checkout master
 npm ci
 npm run build
