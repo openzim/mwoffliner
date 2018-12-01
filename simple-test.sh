b2e2f86a8a82df8a6f26b51031feab77fdbb967e
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-01 14:51:05 +0100

    Fixed travis redis connection

--- simple-test.sh
+++ simple-test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --verbose --format=nozim --articleList=./articleList
+./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --verbose --format=nozim --articleList=./articleList
 mv tmp ../test-run-change
 
 npm i -g mwoffliner
-mwoffliner --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --verbose --format=nozim --articleList=./articleList
+mwoffliner --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --verbose --format=nozim --articleList=./articleList
 mv tmp ../test-run-master
 
 
