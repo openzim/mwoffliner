4ab02464672af6dfb6b69e36d3c62f7b5dd2e36f
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-01 15:58:10 +0100

    Updated test to re-build

--- simple-test.sh
+++ simple-test.sh
@@ -4,6 +4,8 @@
 mv tmp ../test-run-change
 
 git checkout master
+npm ci
+npm run build
 ./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --verbose --format=nozim --articleList=./articleList
 mv tmp ../test-run-master
 
