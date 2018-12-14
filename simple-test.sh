6463384540536e96ea56b850f616a15f79de0d73
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-14 11:59:06 +0000

    Added percy wrapper for mocha and removed stray comment

--- simple-test.sh
+++ simple-test.sh
@@ -4,7 +4,7 @@ cp ./articleList ../articleList
 
 ./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --format=nozim --articleList=../articleList
 
-node_modules/.bin/mocha --exit
+./node_modules/.bin/percy exec -- ./node_modules/.bin/mocha --exit
 
 mv tmp ../test-run-change
 
