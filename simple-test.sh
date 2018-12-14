7add867bf2de6910078379efa5d2b7dec2f004f5
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-14 12:11:18 +0000

    Remove un-necessary double scrape

--- simple-test.sh
+++ simple-test.sh
@@ -7,19 +7,3 @@ cp ./articleList ../articleList
 ./node_modules/.bin/percy exec -- ./node_modules/.bin/mocha --exit
 
 mv tmp ../test-run-change
-
-
-git checkout master
-
-rm -rf cac
-redis-cli flushall
-
-npm ci
-npm run build
-./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --format=nozim --articleList=../articleList
-mv tmp ../test-run-master
-
-
-(diff -r ../test-run-change ../test-run-master)
-
-exit 0
