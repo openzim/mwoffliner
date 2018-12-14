16cb0ea71c4edf988d0aa2f9dab097651437c7c4
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-14 12:11:50 +0000

    Removed extra mov in test

--- simple-test.sh
+++ simple-test.sh
@@ -5,5 +5,3 @@ cp ./articleList ../articleList
 ./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --format=nozim --articleList=../articleList
 
 ./node_modules/.bin/percy exec -- ./node_modules/.bin/mocha --exit
-
-mv tmp ../test-run-change
