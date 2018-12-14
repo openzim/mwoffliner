efaee128e62c4aa4c57d18df4c7acf95e2e260a8
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-14 11:47:44 +0000

    Added exit condition to mocha

--- simple-test.sh
+++ simple-test.sh
@@ -4,7 +4,7 @@ cp ./articleList ../articleList
 
 ./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --format=nozim --articleList=../articleList
 
-node_modules/.bin/mocha
+node_modules/.bin/mocha --exit
 
 mv tmp ../test-run-change
 
