8f5bf315a2d0ee6ae2ef090743fe406abe1f1227
Joe Reeve <joseph.reeve@googlemail.com>
2018-12-18 16:50:26 +0000

    Fix CI

--- simple-test.sh
+++ simple-test.sh
@@ -2,6 +2,6 @@
 cp ./articleList ../articleList
 
 
-./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localParsoid --redis=redis://127.0.0.1:6379 --format=nozim --articleList=../articleList
+./bin/mwoffliner.script.js --mwUrl=https://en.wikipedia.org --adminEmail=admin@kiwix.com --localMcs --redis=redis://127.0.0.1:6379 --format=nozim --articleList=../articleList
 
 ./node_modules/.bin/percy exec -- ./node_modules/.bin/mocha --exit
