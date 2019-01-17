9e446760b12a4703468d745b093eda282390efcc
Joe Reeve <joseph.reeve@googlemail.com>
2019-01-17 08:50:08 +0100

    Fixed type error

--- src/ZimCreatorFs.ts
+++ src/ZimCreatorFs.ts
@@ -8,11 +8,11 @@ class ZimCreatorFs extends ZimCreator {
     public _createZimCreator(fileName: string, welcome: string) {
         try {
             rmdirSync(fileName);
-        } catch (err) { /* NOOP */}
+        } catch (err) { /* NOOP */ }
         mkdirp.sync(fileName);
     }
 
-    public async addArticle(article: ZimArticle) {
+    public async addArticle(this: any, article: ZimArticle) {
         const { dir } = path.parse(article.aid);
         await mkdirp.mkdirp(path.join(this.fileName, dir));
         return writeFilePromise(path.join(this.fileName, article.aid), article.bufferData);
