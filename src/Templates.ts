d4f6baadbad60f4ebd4df8755af3bdcc99aba35a
Joe Reeve <joseph.reeve@googlemail.com>
2019-03-03 15:00:47 +0100

    :bug: More url generation changes and redis flush at beginning of run

--- src/Templates.ts
+++ src/Templates.ts
@@ -8,23 +8,26 @@ function readTemplate(t: string) {
     return readFileSync(pathParser.resolve(__dirname, '../res', t), 'utf-8');
 }
 
-const cssLinks = config.output.cssResources.reduce((buf, css) => {
-    return buf + genHeaderCSSLink(config, css);
-}, '');
-
-const jsScripts = config.output.jsResources.reduce((buf, js) => {
-    return buf + genHeaderScript(config, js);
-}, '');
-
 /* Compile templates */
 const footerTemplate = swig.compile(readTemplate(config.output.templates.footer));
 const leadSectionTemplate = swig.compile(readTemplate(config.output.templates.lead_section_wrapper));
 const sectionTemplate = swig.compile(readTemplate(config.output.templates.section_wrapper));
 const subSectionTemplate = swig.compile(readTemplate(config.output.templates.subsection_wrapper));
 
-const htmlTemplateCode = readTemplate(config.output.templates.page)
-    .replace('__CSS_LINKS__', cssLinks)
-    .replace('__JS_SCRIPTS__', jsScripts);
+const htmlTemplateCode = (articleId: string) => {
+    const cssLinks = config.output.cssResources.reduce((buf, css) => {
+        return buf + genHeaderCSSLink(config, css, articleId);
+    }, '');
+
+    const jsScripts = config.output.jsResources.reduce((buf, js) => {
+        return buf + genHeaderScript(config, js, articleId);
+    }, '');
+
+    return readTemplate(config.output.templates.page)
+        .replace('__CSS_LINKS__', cssLinks)
+        .replace('__JS_SCRIPTS__', jsScripts);
+};
+
 const articleListHomeTemplate = readTemplate(config.output.templates.articleListHomeTemplate);
 
 export {
