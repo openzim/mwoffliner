3e1c8ef3970a2b1a1f73ec186e51bcf747a2804b
Joe Reeve <joseph.reeve@googlemail.com>
2019-06-07 14:47:40 +0100

    :white_check_mark: fixed coverage reporting

--- test/sanity.test.ts
+++ test/sanity.test.ts
@@ -1,6 +1,13 @@
 import test from 'blue-tape';
+import { contains } from 'src/util';
 
 // Super Simple Sanity tests
 test(async (t) => {
     t.ok(true);
+});
+
+test(async (t) => {
+    const arr = [1, 2, 3];
+    const bool = contains(arr, 3);
+    t.ok(bool);
 });
