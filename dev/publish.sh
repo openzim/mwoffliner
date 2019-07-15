30e34a628d159095614ddd36e3728740d8e5a395
Joe Reeve <joseph.reeve@googlemail.com>
2019-07-15 09:35:22 +0100

    :bookmark: update publish config

--- dev/publish.sh
+++ dev/publish.sh
@@ -1,5 +1,7 @@
 # This should only be run by Travis
 if [ -n "$CI" ]; then
+    npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
+    
     if [ -n "$TRAVIS_TAG" ]; then
         echo "Found tag [$TRAVIS_TAG], releasing"
         if [[ $TRAVIS_TAG == *"-rc"* ]]; then
