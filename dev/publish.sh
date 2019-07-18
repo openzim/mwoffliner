a702b4540de224d1e262621624fc067fc08f1b6c
Joe Reeve <joseph.reeve@googlemail.com>
2019-07-18 13:58:15 +0100

    :construction_worker: only do release if building with node 11

--- dev/publish.sh
+++ dev/publish.sh
@@ -1,23 +1,25 @@
 # This should only be run by Travis
 if [ -n "$CI" ]; then
-    npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
+    if [[ "$(node --version)" = *"v11."* ]]; then
+        npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
 
-    if [ -n "$TRAVIS_TAG" ]; then
-        echo "Found tag [$TRAVIS_TAG], releasing"
-        if [[ $TRAVIS_TAG == *"-rc"* ]]; then
-            echo "This is an rc release"
-            npm publish --tag rc
+        if [ -n "$TRAVIS_TAG" ]; then
+            echo "Found tag [$TRAVIS_TAG], releasing"
+            if [[ $TRAVIS_TAG == *"-rc"* ]]; then
+                echo "This is an rc release"
+                npm publish --tag rc
+            else
+                echo "This is a production release"
+                npm publish
+            fi
         else
-            echo "This is a production release"
-            npm publish
-        fi
-    else
-        if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-            echo "No tag found, releasing dev"
-            node dev/hackPackageVersion.js
-            npm publish --tag dev
-        else
-            echo "Not on master, so not releasing dev"
+            if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+                echo "No tag found, releasing dev"
+                node dev/hackPackageVersion.js
+                npm publish --tag dev
+            else
+                echo "Not on master, so not releasing dev"
+            fi
         fi
     fi
 else
