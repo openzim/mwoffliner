02fd1a72e26dc3932498ac181790d51bbea91f00
Emmanuel Engelhart <kelson@kiwix.org>
2019-08-10 20:32:02 +0200

    Update to zimcheck 1.0.4

--- dev/install-zimtools.sh
+++ dev/install-zimtools.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ..
-wget http://download.openzim.org/nightly/2019-08-07/zim-tools_linux-x86_64-2019-08-07.tar.gz
+wget http://download.openzim.org/release/zim-tools/zim-tools_linux-x86_64-1.0.4.tar.gz
 tar xvfz zim-tools*.tar.gz
