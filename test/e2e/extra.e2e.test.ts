78d7bdd2ad94ef4fd707401117b58054634a32d9
Joe Reeve <joseph.reeve@googlemail.com>
2019-08-27 10:02:29 +0100

    :white_check_mark: add "extra" test file for customMainPage

---
+++ test/e2e/extra.e2e.test.ts
@@ -0,0 +1,62 @@
+import test from 'blue-tape';
+import { execute } from '../../src/mwoffliner.lib';
+import { zimcheckAvailable, zimcheck } from '../util';
+import rimraf from 'rimraf';
+import { execPromise, writeFilePromise, mkdirPromise } from '../../src/util';
+import { join } from 'path';
+// import { ZimReader } from '@openzim/libzim';
+// tslint:disable-next-line: no-var-requires
+require('dotenv').config();
+
+const now = new Date();
+const testId = join(process.cwd(), `mwo-test-${+now}`);
+
+const articleListUrl = join(testId, '/articleList');
+
+test.only('Simple customMainPage', async (t) => {
+    await execPromise(`redis-cli flushall`);
+    await mkdirPromise(testId);
+
+    const articleListLines = `
+Book:Cancer
+Book:Ears_nose_throat
+Book:Eye_diseases`;
+
+    await writeFilePromise(articleListUrl, articleListLines, 'utf8');
+
+    const outFiles = await execute({
+        mwUrl: `https://en.wikipedia.org`,
+        adminEmail: `test@kiwix.org`,
+        articleList: articleListUrl,
+        customMainPage: 'Wikipedia:WikiProject_Medicine/Open_Textbook_of_Medicine2',
+        outputDirectory: testId,
+        redis: process.env.REDIS,
+        format: ['nopic'],
+    });
+
+    t.equal(outFiles.length, 1, `Created 1 outputs`);
+
+    for (const dump of outFiles) {
+        if (dump.nopic) {
+            t.equal(dump.status.articles.success, 4, 'nopic has 4 articles');
+        }
+
+        if (await zimcheckAvailable()) {
+            try {
+                await zimcheck(dump.outFile);
+                t.ok(true, `Zimcheck passes`);
+            } catch (err) {
+                t.ok(false, `Zimcheck passes`);
+            }
+        } else {
+            console.log(`Zimcheck not installed, skipping test`);
+        }
+    }
+
+    t.ok(true, 'Scraped customMainPage');
+    // TODO: clear test dir
+    rimraf.sync(testId);
+
+    const redisScan = await execPromise(`redis-cli --scan`);
+    t.equal(redisScan, '', 'Redis has been cleared');
+});
