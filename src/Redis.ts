e20155d75022240142c5feacb55efc8b7d5a4b09
Fontirroig <your_email@example.com>
2019-10-26 15:56:57 -0500

    #531 Should give a nicer error when we can't connect to Redis

--- src/Redis.ts
+++ src/Redis.ts
@@ -1,9 +1,15 @@
 import redis from 'redis';
+import logger from './Logger';
 
 class Redis {
   public client: any;
   constructor(argv: any, config: any) {
     this.client = redis.createClient(argv.redis || config.defaults.redisConfig);
+
+    this.client.on("error", function (err: Error) {
+      logger.error(err.message);
+      process.exit(1);
+    });
   }
 }
 
