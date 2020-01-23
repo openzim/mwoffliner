15d978ffbf458745a79b1798a60b59b5ddebf881
utkarsha <utkarsha.bakshi@gmail.com>
2020-01-23 00:05:47 -0500

    file comparison between aws and local image

--- src/util/aws.ts
+++ src/util/aws.ts
@@ -11,14 +11,15 @@ const s3WasabiConfig = new AWS.S3({
   secretAccessKey: WASABI_CONFIG.SECRET_ACCESS_KEY
 });
 
-export async function uploadImage(imagresponseHeaders: any, filepath: string){
-    logger.log('INSIDE CACHING FUNCTION-------------------------------')
-    fs.writeFile('/tmp/tempFile',imagresponseHeaders.data, function(){
+export async function uploadImage(imagresponse: any, filepath: string){
+    logger.log('INSIDE CACHING FUNCTION-------------------------------', imagresponse.data);
+
+    fs.writeFile('/tmp/tempFile',imagresponse.data, function(){
         //Need to refactor so that params are not declared again and again
         let params = {
             Bucket: WASABI_CONFIG.BUCKET_NAME,
-            ContentType: imagresponseHeaders.headers['content-type'],
-            ContentLength: imagresponseHeaders.headers['content-length'],
+            ContentType: imagresponse.headers['content-type'],
+            ContentLength: imagresponse.headers['content-length'],
             Key: path.basename(filepath),
             Body: fs.createReadStream('/tmp/tempFile')
         };
@@ -29,6 +30,7 @@ export async function uploadImage(imagresponseHeaders: any, filepath: string){
         // };
        
         s3WasabiConfig.putObject(params, function(err, data){
+            logger.log('COMING INSIDE-------PUT ');
             //make the method more generic with just change of s3Obj properties
             if (!err) {
                 logger.log('COMING INSIDE-------SUCCESS', data);
@@ -40,24 +42,35 @@ export async function uploadImage(imagresponseHeaders: any, filepath: string){
 
 }
 
-export async function checkIfImageAlreadyExistsInAws(filepath: string) : Promise<boolean>{
+export async function checkIfImageAlreadyExistsInAws(respFromAxios: any, filepath: string) {
     let params = {
         Bucket:WASABI_CONFIG.BUCKET_NAME,
         Key: path.basename(filepath)
     }
     logger.log(params);
-    return new Promise((resolve, reject) => {
-        const getUrl =  s3WasabiConfig.getObject(params, async (err: any, val: any) =>{
-            if(err){
-                reject(false);
-            } else {
-                logger.log(val)
-                resolve(true);
-            }
-        })
-    });
-   
-   
+    // return new Promise((resolve, reject) => {
+    //     const getUrl =  s3WasabiConfig.getObjectMetadata(params, async (err: any, val: any) =>{
+    //         if(err){
+    //             logger.log('Etag from request dont exist', headers);
+    //             reject(false);
+    //         } else {
+    //             //logger.log('Etag from aws', val.ETag);
+    //             logger.log('Etag from request exist', headers);
+    //             resolve(true);
+    //         }
+    //     })
+    // });
+    try { 
+        const headCode = await s3WasabiConfig.headObject(params).promise();
+        const signedUrl = s3WasabiConfig.getSignedUrl('getObject', params);
+        logger.log('Found', filepath);
+        // Do something with signedUrl
+      } catch (headErr) {
+        if (headErr.code === 'NotFound') {
+          await uploadImage(respFromAxios, filepath);
+          logger.log('Not Found', filepath);
+        }
+      }
 
 }
 
