ff017e3138117528d8e81aab5f191a235b72f648
utkarsha <utkarsha.bakshi@gmail.com>
2020-01-26 17:46:28 -0500

    saving file from aws

--- src/util/const.ts
+++ src/util/const.ts
@@ -4,6 +4,6 @@ export const MIN_IMAGE_THRESHOLD_ARTICLELIST_PAGE = 10;
 export const WASABI_CONFIG = {
     ACCESS_KEY_ID : 'SJGJT2C2H0WM6S1744W1',
     SECRET_ACCESS_KEY: 'oNiEt0YfmZ4IShJBlU7XJu0EmWXtcDwdoKsmQZAC',
-    ENDPOINT_ZONE:'s3.us-east-1.wasabisys.com',
-    BUCKET_NAME: 'poler'
+    ENDPOINT_ZONE:'s3.us-west-1.wasabisys.com',
+    BUCKET_NAME: 'mwoffliner'
 }
