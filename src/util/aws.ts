999851994f9ea5aa8c195fd5019f6c69e4c4266e
utkarsha <utkarsha.bakshi@gmail.com>
2020-01-27 17:57:20 -0500

    with flow working end to end without compressed images

--- src/util/aws.ts
+++ src/util/aws.ts
@@ -2,19 +2,18 @@ import AWS from 'aws-sdk';
 import fs, { readFileSync } from 'fs';
 import * as path from 'path';
 import logger from '../Logger';
-import  {WASABI_CONFIG} from './const';
+import { WASABI_CONFIG } from './const';
 
-const wasabiAwsUrl : any = new AWS.Endpoint(WASABI_CONFIG.ENDPOINT_ZONE);
+const wasabiAwsUrl: any = new AWS.Endpoint(WASABI_CONFIG.ENDPOINT_ZONE);
 const s3WasabiConfig = new AWS.S3({
-  endpoint : wasabiAwsUrl,
-  accessKeyId: WASABI_CONFIG.ACCESS_KEY_ID,
-  secretAccessKey: WASABI_CONFIG.SECRET_ACCESS_KEY
+    endpoint: wasabiAwsUrl,
+    accessKeyId: WASABI_CONFIG.ACCESS_KEY_ID,
+    secretAccessKey: WASABI_CONFIG.SECRET_ACCESS_KEY
 });
 
-export async function uploadImage(imagresponse: any, filepath: string){
-    //logger.log('INSIDE CACHING FUNCTION-------------------------------', imagresponse.data);
-
-    fs.writeFile('/tmp/tempFile',imagresponse.data, function(){
+export async function uploadImage(imagresponse: any, compressedImgData: any, filepath: string) {
+    logger.log('MALI  BAMKAO', compressedImgData, imagresponse.data)
+     fs.writeFile('/tmp/tempFile', imagresponse.data, function () {
         //Need to refactor so that params are not declared again and again
         let params = {
             Bucket: WASABI_CONFIG.BUCKET_NAME,
@@ -23,62 +22,51 @@ export async function uploadImage(imagresponse: any, filepath: string){
             Key: path.basename(filepath),
             Body: fs.createReadStream('/tmp/tempFile')
         };
-    
+
         // var options = {
         //     partSize: 10 * 1024 * 1024, // 10 MB
         //     queueSize: 10
         // };
-       
-        s3WasabiConfig.putObject(params, function(err, data){
-            //logger.log('COMING INSIDE-------PUT ');
+
+        s3WasabiConfig.putObject(params, function (err, data) {
+            logger.log('MALI  BAMKAO PUT');
             //make the method more generic with just change of s3Obj properties
             if (!err) {
-                logger.log('COMING INSIDE-------SUCCESS', data);
+                logger.log('Succefully uploaded the image', data);
             } else {
-                //await getObjectDetails()
-                logger.log('COMING INSIDE-------ERROR', err);
-            }
-        }) 
-    });
-
-}
-
-export async function checkIfImageAlreadyExistsInAws(filepath: string): Promise<any>{
-    let params = {
-        Bucket:WASABI_CONFIG.BUCKET_NAME,
-        Key: path.basename(filepath)
-    }
-    logger.log(params);
-    try{
-        const headCode = await s3WasabiConfig.headObject(params).promise();
-        const signedUrl = s3WasabiConfig.getSignedUrl('getObject', params);
-        //logger.log('head code', headCode);
-        await getObjectDetails(params, headCode);
-    }
-    catch(err){
-
-    }
-
-}
-
-export async function getObjectDetails(params : any, headCode : any) : Promise<any>{
-    return new Promise((resolve, reject) => {
-        const getUrl =  s3WasabiConfig.getObject(params, async (err: any, val: any) =>{
-            //logger.log('Etag fromdfdf djjdj', err);
-            if(err){
-                logger.log('Etag from request dont exist', err);
-                reject(err);
-            } else {
-                //logger.log('Etag from aws', val.ETag);
-                logger.log('Etag from request exist', val);
-
-                resolve({'headers': headCode, 'imgData': val.Body});
+                logger.log(`Not able to upload ${filepath}:`, err);
             }
         })
     });
+
 }
 
-export default{
+export async function checkIfImageAlreadyExistsInAws(filepath: string): Promise<any> {
+    let params = {
+        Bucket: WASABI_CONFIG.BUCKET_NAME,
+        Key: path.basename(filepath)
+    }
+    try {
+        const headCode = await s3WasabiConfig.headObject(params).promise();
+        const signedUrl = s3WasabiConfig.getSignedUrl('getObject', params);
+
+        return new Promise((resolve, reject) => {
+            const getUrl = s3WasabiConfig.getObject(params, async (err: any, val: any) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({ 'headers': headCode, 'imgData': val.Body });
+                }
+            });
+        });
+    }
+    catch (err) {
+
+    }
+}
+
+
+export default {
     uploadImage,
     checkIfImageAlreadyExistsInAws
 }
