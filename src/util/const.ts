797938ad26da04ac43e207d11f02aba76f0e4971
utkarsha <utkarsha.bakshi@gmail.com>
2020-01-30 00:39:15 -0500

    optimisationCacheUrl Param integrated

--- src/util/const.ts
+++ src/util/const.ts
@@ -1,9 +1,3 @@
 
 export const MEDIA_REGEX = /^(.*\/)([^/]+)(\/)(\d+px-|)(.+?)(\.[A-Za-z0-9]{2,6}|)(\.[A-Za-z0-9]{2,6}|)$/;
 export const MIN_IMAGE_THRESHOLD_ARTICLELIST_PAGE = 10;
-export const WASABI_CONFIG = {
-    ACCESS_KEY_ID : 'SJGJT2C2H0WM6S1744W1',
-    SECRET_ACCESS_KEY: 'oNiEt0YfmZ4IShJBlU7XJu0EmWXtcDwdoKsmQZAC',
-    ENDPOINT_ZONE:'s3.us-west-1.wasabisys.com',
-    BUCKET_NAME: 'mwoffliner'
-}
--- src/util/s3.ts
+++ src/util/s3.ts
@@ -2,30 +2,35 @@ import S3File from 'aws-sdk';
 import fs, { readFileSync } from 'fs';
 import * as path from 'path';
 import logger from '../Logger';
-import { WASABI_CONFIG } from './const';
 import axios from 'axios';
 
-const s3Url: any = new S3File.Endpoint(WASABI_CONFIG.ENDPOINT_ZONE);
-const s3Config = new S3File.S3({
-    endpoint: s3Url,
-    accessKeyId: WASABI_CONFIG.ACCESS_KEY_ID,
-    secretAccessKey: WASABI_CONFIG.SECRET_ACCESS_KEY
-});
+let s3Config : any;
+let bucketName: string;
+
+export async function initialiseS3Config(url:string, params:any){
+    const s3UrlBase: any = new S3File.Endpoint(url);
+    s3Config = new S3File.S3({
+        endpoint: s3UrlBase,
+        accessKeyId: params.keyId,
+        secretAccessKey: params.secretAccessKey
+    })
+    bucketName = params.bucketName;
+}
 
 export async function uploadImage(imageResp: any, filepath: string) {
      fs.writeFile('/tmp/tempFile', imageResp.data, function () {
         //Need to refactor so that params are not declared again and again
         let params = {
-            Bucket: WASABI_CONFIG.BUCKET_NAME,
+            Bucket: bucketName,
             ContentType: imageResp.headers['content-type'],
             ContentLength: imageResp.headers['content-length'],
             Key: path.basename(filepath),
-            Metadata: {'etag': imageResp.headers.etag ? imageResp.headers.etag : null},
+            Metadata: {'etag': imageResp.headers.etag },
             Body: fs.createReadStream('/tmp/tempFile')
         };
 
         try{
-            s3Config.putObject(params, function (err, data) {
+            s3Config.putObject(params, function (err: any, data: any) {
                 if (data) {
                     logger.log('Succefully uploaded the image', data);
                 } else {
@@ -42,7 +47,7 @@ export async function uploadImage(imageResp: any, filepath: string) {
 
 export async function existsInS3(filepath: string): Promise<any> {
     let params = {
-        Bucket: WASABI_CONFIG.BUCKET_NAME,
+        Bucket: bucketName,
         Key: path.basename(filepath)
     }
     try {
@@ -72,5 +77,6 @@ export async function existsInS3(filepath: string): Promise<any> {
 
 export default {
     uploadImage,
-    existsInS3
+    existsInS3,
+    initialiseS3Config
 }
