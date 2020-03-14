b624b9b26a062fbe6885a4c498fc7fbc1ca3980c
LakmaNeha <lakma.neha@gmail.com>
2020-03-14 14:10:35 +0530

    resolve custom processor

---
+++ src/util/customFlavour.ts
@@ -0,0 +1,33 @@
+import pathParser from 'path';
+import fs from 'fs';
+
+export function getCustomProcessorPath(customFlavour: string): string {
+
+  /**
+   * Priorities as follows
+   * 1. Absolute Path
+   * 2. Current directory in which command has been run
+   * 3. mwoffliner's extensions directory
+   * 4. Root folder
+   */
+  const possiblePaths = [
+    customFlavour,
+    pathParser.resolve(process.cwd(), customFlavour),
+    pathParser.resolve(process.cwd(), `${customFlavour}.js`),
+    pathParser.resolve(__dirname, `../../extensions/${customFlavour}`),
+    pathParser.resolve(__dirname, `../../extensions/${customFlavour}.js`),
+    pathParser.resolve('/', `${customFlavour}`),
+    pathParser.resolve('/', `${customFlavour}.js`),
+  ];
+
+  let fileFound = false;
+
+  for (const possiblePath of possiblePaths) {
+    if (!fileFound && fs.existsSync(possiblePath)) {
+      fileFound = true;
+      return possiblePath;
+    }
+  }
+
+  return null;
+}
