60c9798db36ff6df69c249d31abdbd90a0395403
Aleksey Gorb <midiman@ya.ru>
2020-04-12 21:58:29 +0300

    fix build

--- dev/build.sh
+++ dev/build.sh
@@ -2,8 +2,8 @@
 echo "Building at [$(date)]"
 npx tsc -p tsconfig.build.json
 # Remove tslint disable
-npx replace "\/\/ tslint:disable-next-line\n" "" ./lib/cli.js 
+npx replace "\/\/ tslint:disable-next-line\n" "" ./lib/src/cli.js
 # typescript compiler adds a semicolon to the second line we need to remove that too
-npx replace "':'; //#" "':' //#" ./lib/cli.js 
-chmod +x ./lib/cli.js
+npx replace "':'; //#" "':' //#" ./lib/src/cli.js
+chmod +x ./lib/src/cli.js
 echo "Build Complete at [$(date)]"
