7a52cbd6592d1a52708cce983a6c5243ba003997
Aleksey Gorb <midiman@ya.ru>
2020-04-16 23:00:10 +0300

    deeper test

--- test/jest/redis.test.ts
+++ test/jest/redis.test.ts
@@ -1,17 +1,62 @@
+// noinspection ES6UnusedImports
 import {} from 'ts-jest';
 import {createClient} from 'redis-mock';
+import {fetchMockData} from './mock/mock';
 import type {RedisClient} from 'redis-mock';
 import {RedisKvs} from '../../src/util/RedisKvs';
-import {fetchMockData} from './mock/mock';
 
 let client: RedisClient;
 let kvs: RedisKvs<any>;
 
+const items = 10000;
+const numWorkers = 24;
 
-const handler = (items: KVS<any>, workerId: number): any => {
-  // console.table(items);
+const intermediateHandler = (items: KVS<any>, workerId: number): any => {
+  // nop
 };
 
+const delayedHandler = async (items: KVS<any>, workerId: number): Promise<any> => {
+  // console.table(items);
+  const t = Math.random() * 20;
+  // console.log(`${workerId} - ${t.toFixed()} ms - ${Object.keys(items).length} items`);
+  return new Promise(((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, t);
+  }));
+};
+
+// tslint:disable-next-line:ban-types
+const getHandler = (handler: (items: KVS<any>, workerId: number) => any | Promise<any>) => async () => {
+  const len = await kvs.len();
+  // @ts-ignore
+  const mockHandler = jest.fn(handler);
+
+  await kvs.iterateItems(numWorkers, mockHandler);
+
+  // ...have been called at all
+  expect(mockHandler).toHaveBeenCalled();
+
+  let count = 0;
+  const workers = new Set();
+  mockHandler.mock.calls
+    .forEach(([items, workerId]) => {
+      count += Object.keys(items).length;
+      workers.add(workerId);
+    });
+
+  // ...iterated over all items
+  expect(count).toEqual(len);
+
+  // ...using proper workers
+  const workersUsed = Array.from(workers) as number[];
+  const workerIdsExpected = Array.from(Array(numWorkers).keys());
+  const workerIdsUnexpected = workersUsed.filter((x) => !workerIdsExpected.includes(x));
+  const workerIdsUnused = workerIdsExpected.filter((x) => !workersUsed.includes(x));
+
+  expect(workerIdsUnused.length).toEqual(0);
+  expect(workerIdsUnexpected.length).toEqual(0);
+};
 
 beforeAll(() => {
   client = createClient();
@@ -21,42 +66,15 @@ beforeAll(() => {
 
 describe('RedisKvs.iterateItems()', () => {
 
-  beforeAll(async () => {
-    client = createClient();
-    kvs = new RedisKvs<{ value: number }>(client, 'test-kvs');
-    await fetchMockData(kvs);
-  });
+  describe(`Workers: ${numWorkers}, items: ${items}`, () => {
 
-  test('works as expected', async () => {
-    const numWorkers = 3;
+    beforeAll(async () => {
+      client = createClient();
+      kvs = new RedisKvs<{ value: number }>(client, 'test-kvs');
+      await fetchMockData(kvs, items);
+    });
 
-    const len = await kvs.len();
-    const mockHandler = jest.fn(handler);
-
-    await kvs.iterateItems(1, mockHandler);
-
-    // ...have been called at all
-    expect(mockHandler).toHaveBeenCalled();
-
-    let count = 0;
-    const workers = new Set();
-    mockHandler.mock.calls
-      .forEach(([items, workerId]) => {
-        count += Object.keys(items).length;
-        workers.add(workerId);
-      });
-
-    // ...iterated over all items
-    expect(count).toEqual(len);
-
-    // ...using proper workers
-    const workersUsed = Array.from(workers) as number[];
-    const workerIdsExpected = Array.from(Array(numWorkers).keys());
-    const workerIdsUnexpected = workersUsed.filter((x) => !workerIdsExpected.includes(x));
-    const workerIdsUnused = workerIdsExpected.filter((x) => !workersUsed.includes(x));
-
-    expect(workerIdsUnused.length).toEqual(0);
-    expect(workerIdsUnexpected.length).toEqual(0);
-
+    test('With intermediate handler', getHandler(intermediateHandler));
+    test('With delayed handler', getHandler(delayedHandler));
   });
 });
