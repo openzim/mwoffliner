119c00cf8e02f4fa4cf4e1592130c31b0d023f76
Aleksey Gorb <midiman@ya.ru>
2020-04-16 20:30:33 +0300

    fix test config / make breakpoints work again

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,17 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  collectCoverage: true,
-};
+  testMatch: ["**/test/jest/*.test.ts"],
+  moduleFileExtensions: ["ts", "js"],
+  collectCoverage: false,
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        sourceMap: true,
+        inlineSourceMap: true
+      }
+    }
+  }
+}
