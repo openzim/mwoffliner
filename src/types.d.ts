7bf3422b7ec6129944a0c6b79fa5bd1d5f62c742
Aleksey Gorb <midiman@ya.ru>
2020-04-18 14:15:48 +0300

    fix TS errors/warnings in type defs

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,155 +1,166 @@
 declare module 'follow-redirects';
-declare module "*.json" {
-    const value: any;
-    export default value;
+declare module '*.json' {
+  const value: any;
+  // noinspection JSDuplicatedDeclaration
+  export default value;
 }
-declare module "service-runner";
-declare module "utf8-binary-cutter";
-declare module "expand-home-dir";
-declare module "swig-templates";
-declare module "mkdirp";
-declare module "country-language";
-declare module "backoff";
-declare module "imagemin-advpng";
-declare module "imagemin-jpegoptim";
+declare module 'service-runner';
+declare module 'utf8-binary-cutter';
+declare module 'expand-home-dir';
+declare module 'swig-templates';
+declare module 'mkdirp';
+declare module 'country-language';
+declare module 'backoff';
+declare module 'imagemin-advpng';
+declare module 'imagemin-jpegoptim';
 
 type DominoElement = any;
 
 type Callback = (err?: any, data?: any, extra?: any) => void;
-type KVS<T> = { [key: string]: T };
+interface KVS<T> { [key: string]: T; }
 
-interface CustomProcessor {
-    shouldKeepArticle?: (articleId: string, doc: Document) => Promise<boolean>;
-    preProcessArticle?: (articleId: string, doc: Document) => Promise<Document>;
-    postProcessArticle?: (articleId: string, doc: Document) => Promise<Document>;
-}
-
-interface MWMetaData {
-    langIso2: string;
-    langIso3: string;
-    title: string;
-    subTitle: string;
-    creator: string;
-    mainPage: string;
-    textDir: TextDirection;
-
-    base: any;
-    wikiPath: any;
-    apiPath: any;
-    domain: any;
-    webUrl: string;
-    apiUrl: string;
-    webUrlPath: string;
-    modulePath: string;
-}
-
-interface ContinueOpts {
-    rdcontinue?: string;
-    continue?: string;
-    // ...
-}
-
-interface QueryContinueOpts {
-    categories: {
-        clcontinue: string;
-    }
-    coordinates: {
-        cocontinue: string;
-    }
-    allpages: {
-        gapcontinue: string;
-    },
-    redirects: {
-        rdcontinue: string;
-    },
-    pageimages: {
-        picontinue: string;
-    },
-}
-
-interface PageInfo {
-    ns?: number;
-    title: string;
-}
 
 type ArticleDetail = PageInfo & {
-    subCategories?: PageInfo[],
-    categories?: PageInfo[],
-    pages?: PageInfo[],
-    thumbnail?: {
-        source: string,
-        height: number,
-        width: number,
-    },
-    coordinates?: string, // coordinates.0.lat;coordinates.0.lon
-    timestamp?: string, // revisions.0.timestamp
-    revisionId?: number, //revisions.0.revid
-    internalThumbnailUrl?: string, // internalThumbnailUrl
-    nextArticleId?: string,
-    prevArticleId?: string,
-    missing?: string,
+  subCategories?: PageInfo[],
+  categories?: PageInfo[],
+  pages?: PageInfo[],
+  thumbnail?: {
+    source: string,
+    height: number,
+    width: number,
+  },
+  coordinates?: string, // coordinates.0.lat;coordinates.0.lon
+  timestamp?: string, // revisions.0.timestamp
+  revisionId?: number, // revisions.0.revid
+  internalThumbnailUrl?: string, // internalThumbnailUrl
+  nextArticleId?: string,
+  prevArticleId?: string,
+  missing?: string,
 };
 
 type QueryCategoriesRet = PageInfo[];
 
 type QueryRevisionsRet = Array<{
-    revid: number,
-    parentid: number,
-    minor: string,
-    user: string,
-    timestamp: string,
-    comment: string,
+  revid: number,
+  parentid: number,
+  minor: string,
+  user: string,
+  timestamp: string,
+  comment: string,
 }>;
 
 type QueryCoordinatesRet = Array<{
-    lat: number,
-    lon: number,
-    primary: string,
-    globe: string,
+  lat: number,
+  lon: number,
+  primary: string,
+  globe: string,
 }>;
 
 type QueryRedirectsRet = PageInfo[];
 
-type TextDirection = "ltr" | "rtl";
+type TextDirection = 'ltr' | 'rtl';
+
 
 interface QueryRet {
-    subCategories?: PageInfo[], // :(
-    categories?: QueryCategoriesRet;
-    revisions?: QueryRevisionsRet;
-    coordinates?: QueryCoordinatesRet;
-    redirects?: QueryRedirectsRet;
+  subCategories?: PageInfo[]; // :(
+  categories?: QueryCategoriesRet;
+  revisions?: QueryRevisionsRet;
+  coordinates?: QueryCoordinatesRet;
+  redirects?: QueryRedirectsRet;
 
-    thumbnail?: {
-        source: string
-        width: number
-        height: number,
-    };
+  thumbnail?: {
+    source: string
+    width: number
+    height: number,
+  };
 
-    missing?: string;
+  missing?: string;
 }
+
 
 interface MwApiQueryResponse {
-    normalized?: Array<{ from: string, to: string }>;
-    pages: {
-        [pageId: string]: PageInfo & QueryRet,
-    };
+  normalized?: Array<{ from: string, to: string }>;
+  pages: {
+    [pageId: string]: PageInfo & QueryRet,
+  };
 }
+
 
 interface MwApiResponse {
-    batchcomplete: string;
-    query: MwApiQueryResponse;
-    continue?: {
-        [key: string]: string;
-    };
-    'query-continue'?: QueryContinueOpts;
-    warnings?: {
-        [key: string]: {
-            [key: string]: string;
-        },
-    };
-    error?: any;
+  batchcomplete: string;
+  query: MwApiQueryResponse;
+  continue?: {
+    [key: string]: string;
+  };
+  'query-continue'?: QueryContinueOpts;
+  warnings?: {
+    [key: string]: {
+      [key: string]: string;
+    },
+  };
+  error?: any;
 }
 
+
 interface QueryMwRet {
-    [articleId: string]: PageInfo & QueryRet;
+  [articleId: string]: PageInfo & QueryRet;
+}
+
+
+interface CustomProcessor {
+  shouldKeepArticle?: (articleId: string, doc: Document) => Promise<boolean>;
+  preProcessArticle?: (articleId: string, doc: Document) => Promise<Document>;
+  postProcessArticle?: (articleId: string, doc: Document) => Promise<Document>;
+}
+
+
+interface MWMetaData {
+  langIso2: string;
+  langIso3: string;
+  title: string;
+  subTitle: string;
+  creator: string;
+  mainPage: string;
+  textDir: TextDirection;
+
+  base: any;
+  wikiPath: any;
+  apiPath: any;
+  domain: any;
+  webUrl: string;
+  apiUrl: string;
+  webUrlPath: string;
+  modulePath: string;
+}
+
+
+interface ContinueOpts {
+  rdcontinue?: string;
+  continue?: string;
+  // ...
+}
+
+
+interface QueryContinueOpts {
+  categories: {
+    clcontinue: string;
+  };
+  coordinates: {
+    cocontinue: string;
+  };
+  allpages: {
+    gapcontinue: string;
+  };
+  redirects: {
+    rdcontinue: string;
+  };
+  pageimages: {
+    picontinue: string;
+  };
+}
+
+
+interface PageInfo {
+  ns?: number;
+  title: string;
 }
