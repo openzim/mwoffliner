dc28824bf14c60d6f6393d09f19b488ffb766de3
utkarshabakshi <utkarsha.bakshi@gmail.com>
2020-05-08 20:02:43 +0000

    skip S3 tests if configuration is not present

--- test/unit/s3.test.ts
+++ test/unit/s3.test.ts
@@ -6,29 +6,33 @@ import 'dotenv/config';
 
 const _test = tapePromise(test);
 
-const s3 = new S3(process.env.BASE_URL_TEST, {
-    bucketName: process.env.BUCKET_NAME_TEST,
-    keyId: process.env.KEY_ID_TEST,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY_TEST,
-});
+_test('S3 checks', async(t) => {
+    if (process.env.BUCKET_NAME_TEST) {
+        const s3 = new S3(process.env.BASE_URL_TEST, {
+            bucketName: process.env.BUCKET_NAME_TEST,
+            keyId: process.env.KEY_ID_TEST,
+            secretAccessKey: process.env.SECRET_ACCESS_KEY_TEST,
+        });
 
-_test('s3 checks', async(t) => {
-    const credentialExists = await s3.initialise();
-    t.equals(credentialExists, true, 'Credentials on S3 exists');
-
-    const bucketExists = s3.bucketExists(process.env.BUCKET_NAME_TEST);
-    t.assert(!!bucketExists, 'Given bucket exists in S3');
-
-    const bucketNotExists = s3.bucketExists('random-string');
-    t.rejects(bucketNotExists, 'Given bucket does not exists in S3');
-
-    const imageExist = await s3.downloadIfPossible('bm.wikipedia.org/static/images/project-logos/bmwiki-2x.png', 'https://bm.wikipedia.org/static/images/project-logos/bmwiki-2x.png');
-    t.assert(!!imageExist, 'Image exists in S3');
-    // Checking the data related to image matches
-    t.equals(imageExist.headers.Metadata.etag, '"aeff-54a391a807034"', 'Etag matches');
-    t.equals(imageExist.headers.ContentLength, 19655, 'Content Length matches');
-    t.equals(imageExist.headers.ContentType, 'application/octet-stream', 'Content Type matches');
-
-    const imageNotExist = await s3.downloadIfPossible('bm.wikipedia.org/static/images/project-logos/polsjsshsgd.png', 'https://bm.wikipedia.org/static/images/project-logos/polsjsshsgd.png');
-    t.equals(imageNotExist, undefined, 'Image doesnt exist in S3');
+        const credentialExists = await s3.initialise();
+        t.equals(credentialExists, true, 'Credentials on S3 exists');
+    
+        const bucketExists = s3.bucketExists(process.env.BUCKET_NAME_TEST);
+        t.assert(!!bucketExists, 'Given bucket exists in S3');
+    
+        const bucketNotExists = s3.bucketExists('random-string');
+        t.rejects(bucketNotExists, 'Given bucket does not exists in S3');
+    
+        const imageExist = await s3.downloadIfPossible('bm.wikipedia.org/static/images/project-logos/bmwiki-2x.png', 'https://bm.wikipedia.org/static/images/project-logos/bmwiki-2x.png');
+        t.assert(!!imageExist, 'Image exists in S3');
+        // Checking the data related to image matches
+        t.equals(imageExist.headers.Metadata.etag, '"aeff-54a391a807034"', 'Etag matches');
+        t.equals(imageExist.headers.ContentLength, 19655, 'Content Length matches');
+        t.equals(imageExist.headers.ContentType, 'application/octet-stream', 'Content Type matches');
+    
+        const imageNotExist = await s3.downloadIfPossible('bm.wikipedia.org/static/images/project-logos/polsjsshsgd.png', 'https://bm.wikipedia.org/static/images/project-logos/polsjsshsgd.png');
+        t.equals(imageNotExist, undefined, 'Image doesnt exist in S3');
+    } else {
+        logger.log('Skip S3 tests')
+    }
 });
