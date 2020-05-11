49aa657d14615a8a559a85af5bc31da29b0f061e
Aleksey Gorb <midiman@ya.ru>
2020-05-12 00:00:21 +0300

    migrate to sharp
    Signed-off-by: Aleksey Gorb <midiman@ya.ru>

--- src/mwoffliner.lib.ts
+++ src/mwoffliner.lib.ts
@@ -15,7 +15,6 @@ import * as path from 'path';
 import axios from 'axios';
 import { ZimCreator, ZimArticle } from '@openzim/libzim';
 import rimraf from 'rimraf';
-import im from 'imagemagick';
 import * as QueryStringParser from 'querystring';
 
 import { config } from './config';
@@ -34,6 +33,8 @@ import { getCategoriesForArticles, trimUnmirroredPages } from './util/categories
 import { filesToDownloadXPath, populateFilesToDownload, articleDetailXId, populateArticleDetail, populateRedirects, filesToRetryXPath, populateFilesToRetry, redirectsXId } from './stores';
 import S3 from './S3';
 import { getTotalArticlesNumberByNS } from './util/mw-api';
+import sharp from 'sharp';
+
 const packageJSON = JSON.parse(readFileSync(path.join(__dirname, '../package.json'), 'utf8'));
 
 function closeRedis(redis: Redis) {
@@ -510,19 +511,19 @@ async function execute(argv: any) {
   async function saveFavicon(dump: Dump, zimCreator: ZimCreator): Promise<{}> {
     logger.log('Saving favicon.png...');
 
-    async function resizeFavicon(zimCreator: ZimCreator, faviconPath: string): Promise<{}> {
+    async function saveFavicon(zimCreator: ZimCreator, faviconPath: string): Promise<{}> {
       try {
-        await util.promisify(im.convert)([faviconPath, '-thumbnail', '48', faviconPath]);
+        const source = await fs.promises.readFile(faviconPath);
+        const data = await sharp(source).resize(48, 48).toBuffer();
+        const article = new ZimArticle({ url: 'favicon', mimeType: 'image/png', data, ns: '-' });
+        return zimCreator.addArticle(article);
       } catch (e) {
-        throw new Error('Failed to convert favicon using imagemagick');
+        throw new Error('Failed to save favicon using sharp');
       }
-      const data = await readFilePromise(faviconPath, null);
-      const article = new ZimArticle({ url: 'favicon', mimeType: 'image/png', data, ns: '-' });
-      return zimCreator.addArticle(article);
     }
 
     if (customZimFavicon) {
-      return await resizeFavicon(zimCreator, customZimFavicon);
+      return await saveFavicon(zimCreator, customZimFavicon);
     }
     const body = await downloader.getJSON<any>(mw.siteInfoUrl());
     const entries = body.query.general;
@@ -534,21 +535,10 @@ async function execute(argv: any) {
     const ext = parsedUrl.pathname.split('.').slice(-1)[0];
 
     const faviconPath = pathParser.join(tmpDirectory, `favicon.${ext}`);
-    let faviconFinalPath = pathParser.join(tmpDirectory, `favicon.png`);
     const logoUrl = parsedUrl.protocol ? entries.logo : 'http:' + entries.logo;
     const logoContent = await downloader.downloadContent(logoUrl);
     await writeFilePromise(faviconPath, logoContent.content, null);
-    if (ext !== 'png') {
-      logger.info(`Original favicon is not a PNG ([${ext}]). Converting it to PNG`);
-      try {
-        await util.promisify(im.convert)([faviconPath, faviconFinalPath]);
-      } catch (e) {
-        throw new Error('Failed to convert favicon using imagemagick');
-      }
-    } else {
-      faviconFinalPath = faviconPath;
-    }
-    return await resizeFavicon(zimCreator, faviconFinalPath);
+    return await saveFavicon(zimCreator, faviconPath);
   }
 
   function getMainPage(dump: Dump, zimCreator: ZimCreator) {
