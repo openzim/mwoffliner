1e95fbdd3864c86985e1c212140a7d587c7ed945
Aleksey Gorb <midiman@ya.ru>
2020-05-17 19:59:25 +0300

    have to put some magic into logger
    Signed-off-by: Aleksey Gorb <midiman@ya.ru>

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,13 +1,30 @@
-import Pino from 'pino';
+import pino from 'pino';
 
-const Logger = Pino({
+
+const logger = pino({
   level: 'debug',
   // prettyPrint: {
   //   crlf: true
   // },
   customLevels: {
     verbose: 35
+  },
+  // @ts-ignore
+  hooks: {
+    logMethod (inputArgs: any[], method: any) {
+      if (inputArgs.length < 2) {
+        return method.apply(this, inputArgs);
+      }
+      const arg1 = inputArgs.shift();
+      const arg2 = inputArgs.shift();
+      return method.apply(this, [arg2, arg1, ...inputArgs]);
+    }
+  },
+  formatters: {
+    bindings(bindings) {
+      return {};
+    }
   }
 });
 
-export default Logger.pino;
+export default logger;
