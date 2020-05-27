3c070c9b0b02284e626eeb3c8bdc431ee35a8e51
Aleksey Gorb <midiman@ya.ru>
2020-05-27 15:35:02 +0300

    remove unused test

--- test/unit/util.test.ts
+++ test/unit/util.test.ts
@@ -1,34 +1,7 @@
 import './bootstrap.test';
 import test from 'blue-tape';
-import { throttle, sanitizeString, encodeArticleIdForZimHtmlUrl, interpolateTranslationString } from 'src/util';
-import { sleep } from 'test/util';
+import { encodeArticleIdForZimHtmlUrl, interpolateTranslationString } from 'src/util';
 
-test('util -> Throttle', async (t) => {
-    let calledCount = 0;
-    const func = throttle(() => {
-        calledCount += 1;
-    }, 200);
-    t.equal(calledCount, 0, 'Call count is 0 before calling func');
-    func();
-
-    await sleep(100);
-
-    t.equal(calledCount, 1, 'Call count is 1 after calling once');
-
-    await sleep(100);
-
-    func();
-    func();
-    func();
-    func();
-
-    await sleep(100);
-    t.equal(calledCount, 2, 'Call count is 2 after four times in under 200ms');
-
-    t.equals(sanitizeString('Mediawiki <script></script>'), 'Mediawiki  script   script ', 'Escaping HTML Tags');
-    t.equals(sanitizeString('SELECT * FROM db WHERE something="Poler"'), 'SELECT   FROM db WHERE something  Poler ', 'Escaping query characters');
-
-});
 
 test('util -> interpolateTranslationString', async (t) => {
     t.equals(interpolateTranslationString('Hello world', {}), 'Hello world');
