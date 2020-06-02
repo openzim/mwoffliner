b9ac8f23a48d21deedc8ca80b8a372bd290546a7
Aleksey Gorb <midiman@ya.ru>
2020-06-02 13:19:47 +0300

    renderer memory optimizations

--- src/Article/ArticleRenderer.ts
+++ src/Article/ArticleRenderer.ts
@@ -1,16 +1,9 @@
-import domino from 'domino';
-
-
 export class ArticleRenderer {
   // public abstract render(article: Article): Promise<RenderedArticle[]>;
 
-  protected static getStrippedTitleFromHtml(html: string) {
-    const doc = domino.createDocument(html);
-    const titleEl = doc.querySelector('title');
-    if (titleEl) {
-      return titleEl.textContent;
-    } else {
-      return '';
-    }
+  protected static getStrippedTitleFromHtml(html: string): string {
+    const title = /<title>(.*)<\/title>/.exec(html.substr(0, 1024));
+    if (!title) return '';
+    return title.shift();
   }
 }
