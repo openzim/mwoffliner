101852597725ec9f38ad038c875a7edc1bdd9a64
Kelson <kelson@kiwix.org>
2020-07-08 10:02:48 +0200

    Reduce batch size to avoid too long URLs

--- src/util/mw-api.ts
+++ src/util/mw-api.ts
@@ -8,7 +8,7 @@ export async function getArticlesByIds(_articleIds: string[], downloader: Downlo
     let from = 0;
     let numArticleIds = _articleIds.length;
     let numThumbnails = 0;
-    let batchSize = 50;
+    let batchSize = 40;
 
     // using async iterator to spawn workers
     await pmap(
