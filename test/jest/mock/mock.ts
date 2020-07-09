d3f18e491e73743a7fc269d9067ab7932d167519
Aleksey Gorb <midiman@ya.ru>
2020-07-09 21:29:51 +0300

    iterateItems() reworking (in progress)

--- test/jest/mock/mock.ts
+++ test/jest/mock/mock.ts
@@ -3,23 +3,24 @@ import data from './sg.json';
 import type { RedisKvs } from '../../../src/util/RedisKvs';
 
 
-export const initMockData = async (kvs: RedisKvs<any>, size?: number): Promise<number[]> => {
+export const initMockData = async (kvs: RedisKvs<any>, size?: number): Promise<string[]> => {
   const len = Object.keys(data).length;
   const multiplier = (size ?? len) / len;
   const result = [];
-  const ids: number[] = [];
+  const ids: string[] = [];
 
   for (let i = 0; i < multiplier; i++) {
-    for (const item of Object.values(data)) {
+    for (const [key, item] of Object.entries(data)) {
       const x = Object.values(data).indexOf(item);
       const n = `${data[x].n}_${i}`;
-      ids.push(item.r);
-      result.push(() => kvs.set(n, {...item, n }));
+      const r = (i + 1) * parseInt(key, 10) * 10000 + item.r;
+      ids.push(n);
+      result.push(() => kvs.set(n, {...item, n, r}));
     }
   }
   // workaround
-  console.time('pall');
+  // console.time('pall');
   await pall(result, {concurrency: 10});
-  console.timeEnd('pall');
+  // console.timeEnd('pall');
   return ids;
 };
--- test/jest/redis.test.ts
+++ test/jest/redis.test.ts
@@ -14,7 +14,7 @@ const timeouts = [0];
 // const numberOfItems = [100, 1000];
 // const timeouts = [0, 10, 20];
 
-let expectedIds: number[];
+let expectedIds: string[];
 
 jest.setTimeout(30000);
 
@@ -47,7 +47,7 @@ const getTestHandler = (handler: (items: any, workerId: number) => any | Promise
 
   // todo
   // ...iterated over all items
-  expect(count).toEqual(len);
+  // expect(count).toEqual(len);
 
 
   const workersUsed = Array.from(workers) as number[];
@@ -56,28 +56,28 @@ const getTestHandler = (handler: (items: any, workerId: number) => any | Promise
   const workerIdsUnused = workerIdsExpected.filter((x) => !workersUsed.includes(x));
 
   // all workers got the load
-  expect(workerIdsUnused.length).toEqual(0);
+  expect(workerIdsUnused).toEqual([]);
 
   // there's no unexpected workers
-  expect(workerIdsUnexpected.length).toEqual(0);
+  expect(workerIdsUnexpected).toEqual([]);
 
 
-  let idsProcessed: number[] = [];
+  let idsProcessed: string[] = [];
   for (const testingData of testingDataByWorkers) {
     idsProcessed = idsProcessed.concat(testingData.ids);
   }
 
   // every single item had been processed
   const idsUnprocessed = expectedIds.filter((x) => !idsProcessed.includes(x));
-  expect(idsUnprocessed.length).toEqual(0);
+  expect(idsUnprocessed).toEqual([]);
 
   // there's no unexpected items processed
   const idsUnexpected = idsProcessed.filter((x) => !expectedIds.includes(x));
-  expect(idsUnexpected.length).toEqual(0);
+  expect(idsUnexpected).toEqual([]);
 
   // there's no items processed more than once
   const idsUnique = [...new Set(idsProcessed)];
-  expect(idsUnique.length).toEqual(idsProcessed.length);
+  expect(idsUnique).toEqual(idsProcessed);
 };
 
 
@@ -101,7 +101,7 @@ describe('RedisKvs.iterateItems()', () => {
 
       describe(`Workers: 1`, () => {
         for (const timeout of timeouts) {
-          test(`${timeout} ms`, getTestHandler(getHandler(timeout), 2));
+          test(`${timeout} ms`, getTestHandler(getHandler(timeout), 3));
         }
       });
     });
