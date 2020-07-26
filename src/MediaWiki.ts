1718e5b3d1fb489cd7e50e026032e449fdc2214b
utkarshabakshi <utkarsha.bakshi@gmail.com>
2020-07-26 02:43:58 -0400

    broken api url fixed

--- src/MediaWiki.ts
+++ src/MediaWiki.ts
@@ -34,11 +34,11 @@ class MediaWiki {
 
     this.baseUrl = new URL(ensureTrailingChar(config.base, '/'));
 
-    this.apiPath = config.apiPath ?? 'w/api.php?';
+    this.apiPath = config.apiPath ?? 'w/api.php';
     this.wikiPath = config.wikiPath ?? 'wiki/';
 
     this.webUrl = new URL(this.wikiPath, this.baseUrl);
-    this.apiUrl = new URL(this.apiPath, this.baseUrl);
+    this.apiUrl = new URL(`${this.apiPath}?`, this.baseUrl);
 
     this.veApiUrl = new URL(`${this.apiUrl.href}action=visualeditor&mobileformat=html&format=json&paction=parse&page=`);
 
