0d8dbc1d449cf231e38bded5f984bb0c42e8b1cb
manan jethwani <mananjethwani02@gmail.com>
2020-09-22 02:34:26 +0530

    fixed automated test failing due to lack of offline.css

--- test/unit/styles.test.ts
+++ test/unit/styles.test.ts
@@ -5,6 +5,7 @@ import { setupScrapeClasses } from 'test/util';
 import { articleDetailXId } from 'src/stores';
 import { getAndProcessStylesheets, mwRetToArticleDetail } from 'src/util';
 import Axios from 'axios';
+import logger from '../../src/Logger';
 
 test('Stylesheet downloading', async (t) => {
     const { downloader, mw, dump } = await setupScrapeClasses(); // en wikipedia
@@ -17,14 +18,42 @@ test('Stylesheet downloading', async (t) => {
     const offlineCSSUrl = `https://en.wikipedia.org/w/index.php?title=Mediawiki:offline.css&action=raw`;
     const siteStylesUrl = `http://en.wikipedia.org/w/load.php?lang=en&modules=site.styles&only=styles&skin=vector`;
 
-    const { data: offlineCSSContent } = await Axios.get(offlineCSSUrl);
-    const { data: siteStylesContent } = await Axios.get(siteStylesUrl);
+    await Axios.get(offlineCSSUrl)
+        .then(async (resp) => {
+            const offlineCSSContent=resp.data;
 
-    const { finalCss } = await getAndProcessStylesheets(downloader, [offlineCSSUrl, siteStylesUrl]);
+            await Axios.get(siteStylesUrl)
+                .then(async (resp) =>{
+                    const siteStylesContent=resp.data;
+                    const { finalCss } = await getAndProcessStylesheets(downloader, [offlineCSSUrl, siteStylesUrl]);
 
-    t.assert(finalCss.includes(offlineCSSUrl), `Contains offline CSS url`);
-    t.assert(finalCss.includes(offlineCSSContent), `Contains offline CSS content`);
+                    t.assert(finalCss.includes(offlineCSSUrl), `Contains offline CSS url`);
+                    t.assert(finalCss.includes(offlineCSSContent), `Contains offline CSS content`);
+        
+                    t.assert(finalCss.includes(siteStylesUrl), `Contains site CSS url`);
+                    t.assert(!finalCss.includes(siteStylesContent), `Contains re-written site CSS content`);
 
-    t.assert(finalCss.includes(siteStylesUrl), `Contains site CSS url`);
-    t.assert(!finalCss.includes(siteStylesContent), `Contains re-written site CSS content`);
+                })
+                .catch(async (err) => {
+                    logger.log(err.message, ' for ', siteStylesUrl)
+                    const { finalCss } = await getAndProcessStylesheets(downloader, [offlineCSSUrl]);
+
+                    t.assert(finalCss.includes(offlineCSSUrl), `Contains offline CSS url`);
+                    t.assert(finalCss.includes(offlineCSSContent), `Contains offline CSS content`);
+                })
+        })
+        .catch( async (err) => {
+            logger.log(err.message, ' for ', offlineCSSUrl)
+            await Axios.get(siteStylesUrl)
+                .then(async (resp) => {
+                    const siteStylesContent=resp.data;
+                    const { finalCss } = await getAndProcessStylesheets(downloader, [siteStylesUrl]);
+
+                    t.assert(finalCss.includes(siteStylesUrl), `Contains site CSS url`);
+                    t.assert(!finalCss.includes(siteStylesContent), `Contains re-written site CSS content`);
+                })
+                .catch(async (err) => {
+                    logger.log(err.message, ' for ', siteStylesUrl)
+                })
+        });
 });
