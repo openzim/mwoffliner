39df67970f2abcddaba4aa316177d23a87a9c213
manan jethwani <mananjethwani02@gmail.com>
2020-10-27 21:01:40 +0530

    added polyfill script

--- res/script.js
+++ res/script.js
@@ -18,3 +18,36 @@ window.onload = function () {
     
     document.querySelector('body').setAttribute('data-useragent',  navigator.userAgent);
 }
+
+// Location of external scripts to load conditionally
+var webHeroScripts = ['../-/j/js_modules/webpHeroPolyfill.js', '../-/j/js_modules/webpHeroBundle.js'];
+        
+var testWebP = function(callback) {
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height === 2);
+    };
+    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+};
+
+var startWebpMachine = function() {
+    var newScript = document.createElement('script');
+    var inlineScript = document.createTextNode('var webpMachine = new webpHero.WebpMachine(); webpMachine.polyfillDocument()');
+    newScript.appendChild(inlineScript); 
+    document.getElementsByTagName('body')[0].appendChild(newScript);
+};
+
+testWebP(function(support) {
+    if (!support) {
+        webHeroScripts.forEach(function(scriptUrl) {
+            var script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = scriptUrl;
+            if (webHeroScripts[webHeroScripts.length-1] === scriptUrl) {
+                // Start webpMachine if we have loaded the last script
+                script.onload = startWebpMachine;
+            }
+            document.getElementsByTagName('body')[0].appendChild(script);
+        });
+    }
+});
