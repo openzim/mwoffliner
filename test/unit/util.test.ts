53bd09fa9731d46966bda1faf93893a3ce62e0e6
Emmanuel Engelhart <kelson@kiwix.org>
2020-11-12 11:46:04 +0100

    Improve test for getMediaBase()

--- test/unit/util.test.ts
+++ test/unit/util.test.ts
@@ -101,12 +101,14 @@ test('Custom flavour path', async(t) => {
 })
 
 test('getMediaBase tests', async(t) => {
-    t.equal(getMediaBase('https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Westminstpalace.jpg/'
-            + '220px-Westminstpalace.jpg', true),
-        getMediaBase('https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Westminstpalace.jpg/'
-            + '110px-Westminstpalace.jpg', true),
-        'Image size removal.');
 
-    t.equal(getMediaBase('https://wikimedia.org/api/rest_v1/media/math/render/svg/da47d67ac8dcb0be8b68d7bfdc676d9ce9bf1606',true),
-        'm/da47d67ac8dcb0be8b68d7bfdc676d9ce9bf1606.svg', 'Equation filename check.');
+    // Thumbs
+    t.equal(getMediaBase('https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Westminstpalace.jpg/220px-Westminstpalace.jpg', true), 'm/Westminstpalace.jpg', 'Thumb 1');
+    t.equal(getMediaBase('https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Westminstpalace.jpg/110px-Westminstpalace.jpg', true), 'm/Westminstpalace.jpg', 'Thumb 2');
+    t.equal(getMediaBase('https://upload.wikimedia.org/wikipedia/commons/3/39/Westminstpalace.jpg', true), 'm/Westminstpalace.jpg', 'No thumb');
+    t.equal(getMediaBase('https://upload.wikimedia.org/wikipedia/commons/0/0d/VFPt_Solenoid_correct2.svg', true), 'm/VFPt_Solenoid_correct2.svg', 'SVG');
+    t.equal(getMediaBase('https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/VFPt_Solenoid_correct2.svg/120px-VFPt_Solenoid_correct2.svg.png', true), 'm/VFPt_Solenoid_correct2.svg.png', 'SVG');
+
+    // Latex (equations)
+    t.equal(getMediaBase('https://wikimedia.org/api/rest_v1/media/math/render/svg/da47d67ac8dcb0be8b68d7bfdc676d9ce9bf1606', true), 'm/da47d67ac8dcb0be8b68d7bfdc676d9ce9bf1606.svg', 'Latex');
 })
