0b594ccf393b6dd93d86d082adc5bafb75fef453
Manan Jethwani <50267986+MananJethwani@users.noreply.github.com>
2020-11-17 18:37:40 +0530

    added condition to remove a para when only summary present (#1307)
    * added condition to remove a para when only summary present
    
    * used more elegant way for detecting empty para

--- src/util/saveArticles.ts
+++ src/util/saveArticles.ts
@@ -771,7 +771,7 @@ export function applyOtherTreatments(parsoidDoc: DominoElement, dump: Dump) {
         // Desktop view === section
         const sections: DominoElement[] = Array.from(parsoidDoc.querySelectorAll('details, section'));
         for (const section of sections) {
-            if (section.children.length < 1) {
+            if (section.children.length === section.querySelectorAll('summary').length) {
                 DU.deleteNode(section);
             }
         }
