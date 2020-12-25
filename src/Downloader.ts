7d0b670e9e975ed894bb32a0e4bb4e7ee1b8a885
Manan Jethwani <mananjethwani02@gmail.com>
2020-12-25 15:14:04 +0530

    removed accept header from ArrayBufferRequestOption

--- src/Downloader.ts
+++ src/Downloader.ts
@@ -149,7 +149,6 @@ class Downloader {
 
     this.arrayBufferRequestOptions = {
       headers: {
-        'accept': 'text/html; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/HTML/1.8.0"',
         'cache-control': 'public, max-stale=86400',
         'user-agent': this.uaString,
         'cookie': this.loginCookie,
