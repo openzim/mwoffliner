1cfb9f27c0da00f50d137681e26ea6d832c18dd2
Manan Jethwani <mananjethwani02@gmail.com>
2021-03-15 02:45:00 +0530

    removed ./ from articleListMainPage.ts

--- src/util/articleListMainPage.ts
+++ src/util/articleListMainPage.ts
@@ -2,11 +2,11 @@ import { Dump } from '../Dump';
 import { encodeArticleIdForZimHtmlUrl, isWebpCandidateImageUrl } from '../util';
 
 export function makeArticleListItem(dump: Dump, articleEntry: any) {
-    return `<li><a href="${encodeArticleIdForZimHtmlUrl('./' + articleEntry.title.replace(/ /g, '_'))}">${articleEntry.title.replace(/_/g, ' ')}<a></li>\n`;
+    return `<li><a href="${encodeArticleIdForZimHtmlUrl(articleEntry.title.replace(/ /g, '_'))}">${articleEntry.title.replace(/_/g, ' ')}<a></li>\n`;
 }
 
 export function makeArticleImageTile(dump: Dump, articleEntry: ArticleDetail, webp: boolean) {
-    return `<a class="item" href="${encodeArticleIdForZimHtmlUrl('./' + articleEntry.title.replace(/ /g, '_'))}">
+    return `<a class="item" href="${encodeArticleIdForZimHtmlUrl(articleEntry.title.replace(/ /g, '_'))}">
             <figure><img loading="lazy" src="${articleEntry.internalThumbnailUrl +
             (webp && isWebpCandidateImageUrl(articleEntry.internalThumbnailUrl) ? '.webp' : '')}" />
             <figcaption>${(articleEntry.title || '').replace(/_/g, ' ')}</figcaption></figure></a>\n`;
