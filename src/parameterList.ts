38ee8b72a1e98a7614b0775039e0a844ebbd2b05
Emmanuel Engelhart <kelson@kiwix.org>
2022-07-10 11:19:23 +0200

    Remove --zstd option (always using zstd)

--- src/parameterList.ts
+++ src/parameterList.ts
@@ -36,5 +36,4 @@ export const parameterDescriptions = {
   osTmpDir: 'Override default operating system temporary directory path environment variable',
   customFlavour: 'A custom processor that can filter and process articles (see extensions/*.js)',
   optimisationCacheUrl: 'S3 url, including credentials and bucket name',
-  zstd: 'Use Zstandard as ZIM compression (Lzma otherwise)',
 };
