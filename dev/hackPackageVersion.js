96b0adaad046dfc1bdbcce52377cfebf88b61288
Emmanuel Engelhart <kelson@kiwix.org>
2023-02-05 15:56:40 +0100

    Remove deprecated js script

--- dev/hackPackageVersion.js
+++
@@ -1,5 +0,0 @@
-const { writeFileSync } = require('fs');
-
-const pkg = require('../package.json');
-pkg.version = pkg.version + '-' + Date.now();
-writeFileSync('./package.json', JSON.stringify(pkg), 'utf8');
