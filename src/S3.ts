b4adbaf2efbb6724e9bc5a64bfbcc2edf524457a
Manav Goel <manav.goel@paytm.com>
2023-05-25 00:52:24 +0530

    eslint error resolved

--- src/S3.ts
+++ src/S3.ts
@@ -1,4 +1,4 @@
-import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand, HeadBucketCommand } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand, HeadBucketCommand } from '@aws-sdk/client-s3'
 import * as logger from './Logger.js'
 import { Readable } from 'stream'
 import { publicIpv4 } from 'public-ip'
@@ -16,7 +16,7 @@ class S3 {
   }
 
   public async initialise() {
-    const s3UrlBase: any = new URL(this.url);
+    const s3UrlBase: any = new URL(this.url)
     this.s3Handler = new S3Client({
       credentials: {
         accessKeyId: this.params.keyId,
@@ -24,91 +24,91 @@ class S3 {
       },
       endpoint: s3UrlBase.href,
       forcePathStyle: s3UrlBase.protocol === 'http:',
-    });
+    })
 
     return this.bucketExists(this.bucketName)
-        .then(() => true)
-        .catch((err) => {
-          throw new Error(`Unable to connect to S3, either S3 login credentials are wrong or bucket cannot be found
+      .then(() => true)
+      .catch(() => {
+        throw new Error(`Unable to connect to S3, either S3 login credentials are wrong or bucket cannot be found
                             Bucket used: ${this.bucketName}
                             End point used: ${s3UrlBase.href}
-                            Public IP used: ${publicIpv4()}`);
-        });
+                            Public IP used: ${publicIpv4()}`)
+      })
   }
 
   public bucketExists(bucket: string): Promise<any> {
-    const command = new HeadBucketCommand({ Bucket: bucket });
-    return new Promise((resolve,rejecr)=>{
-      this.s3Handler.send(command,(err)=>{
+    const command = new HeadBucketCommand({ Bucket: bucket })
+    return new Promise((resolve, reject) => {
+      this.s3Handler.send(command, (err) => {
         return err ? reject(err) : resolve(true)
       })
     })
   }
 
-  public uploadBlob(key: string, data: any, eTag: string, contentType: string, version: string){
+  public uploadBlob(key: string, data: any, eTag: string, contentType: string, version: string) {
     const command = new PutObjectCommand({
       Bucket: this.bucketName,
       Key: key,
       Metadata: { etag: eTag, contenttype: contentType, version },
       Body: this.bufferToStream(data),
-    });
+    })
 
-    try{
+    try {
       this.s3Handler.send(command, (err) => {
         if (err) {
           logger.log(`Not able to upload ${key}: ${err}`)
         }
       })
-    } catch(err)  {
+    } catch (err) {
       logger.log('S3 error', err)
-        };
+    }
   }
 
   public downloadBlob(key: string, version = '1'): Promise<any> {
-    const command = new GetObjectCommand({ Bucket: this.bucketName, Key: key });
+    const command = new GetObjectCommand({ Bucket: this.bucketName, Key: key })
 
-    return new Promise((resolve,reject)=>{
-      this.s3Handler.send(command)
-          .then((response) => {
-            if (response) {
-              const { Metadata } = response;
-              if (Metadata?.version !== version) {
-                Metadata.etag = undefined;
-              }
-              resolve(response);
-            }else
-              reject()
-          })
-          .catch((err) => {
-            if (err && err.statusCode === 404) {
-              resolve(null)
-            } else {
-              reject(err)
+    return new Promise((resolve, reject) => {
+      this.s3Handler
+        .send(command)
+        .then((response) => {
+          if (response) {
+            const { Metadata } = response
+            if (Metadata?.version !== version) {
+              Metadata.etag = undefined
             }
-          });
+            resolve(response)
+          } else reject()
+        })
+        .catch((err) => {
+          if (err && err.statusCode === 404) {
+            resolve(null)
+          } else {
+            reject(err)
+          }
+        })
     })
   }
 
   // Only for testing purpose
   public deleteBlob(key: any): Promise<any> {
-    const command = new DeleteObjectCommand({ Bucket: this.bucketName, Key: key });
+    const command = new DeleteObjectCommand({ Bucket: this.bucketName, Key: key })
 
     return new Promise((resolve, reject) => {
-      this.s3Handler.send(command)
-          .then((val) => resolve(val))
-          .catch((err) => reject(err))
+      this.s3Handler
+        .send(command)
+        .then((val) => resolve(val))
+        .catch((err) => reject(err))
     })
-
   }
 
   private bufferToStream(binary: Buffer) {
     return new Readable({
       read() {
-        this.push(binary);
-        this.push(null);
+        this.push(binary)
+        this.push(null)
       },
-    });
+    })
   }
 }
 
-export default S3;
+export default S3
