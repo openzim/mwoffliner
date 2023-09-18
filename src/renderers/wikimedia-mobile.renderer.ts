d92008cbe2e4c268914db24e5743c77d3478b566
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2023-09-18 17:50:58 +0300

    Apply mobile css and js in mobile renderer

--- src/renderers/wikimedia-mobile.renderer.ts
+++ src/renderers/wikimedia-mobile.renderer.ts
@@ -1,3 +1,4 @@
+import * as domino from 'domino'
 import * as logger from '../Logger.js'
 import { Renderer } from './abstract.renderer.js'
 import { getStrippedTitleFromHtml } from '../util/misc.js'
@@ -24,14 +25,14 @@ export class WikimediaMobileRenderer extends Renderer {
 
       const displayTitle = this.getStrippedTitle(renderOpts)
       if (data) {
-        // TODO: Apply mobile page transformations before applying other treatments
-        const { subtitles, mediaDependencies } = await super.processHtml(data, dump, articleId, articleDetail, _moduleDependencies, webp)
-
-        // TODO: styles, scripts and most of content are not visible in Kiwix app, but enabled when use Kiwix server
+        const { finalHTML, subtitles, mediaDependencies } = await super.processHtml(data, dump, articleId, articleDetail, _moduleDependencies, webp)
+        // TODO: Add mobile scripts after all treatments but this need to be refactored
+        // TODO: enable reference list
+        const dataWithMobileModules = this.addMobileModules(finalHTML)
         result.push({
           articleId,
           displayTitle,
-          html: data,
+          html: dataWithMobileModules,
           mediaDependencies,
           subtitles,
         })
@@ -41,5 +42,31 @@ export class WikimediaMobileRenderer extends Renderer {
       logger.error(err.message)
       throw new Error(err.message)
     }
+  }
+
+  private addMobileModules(data) {
+    const doc = domino.createDocument(data)
+    const protocol = 'https://'
+    // TODO: query this instead of hardcoding.
+    const offlineResourcesCSSList = [
+      'meta.wikimedia.org/api/rest_v1/data/css/mobile/base',
+      'meta.wikimedia.org/api/rest_v1/data/css/mobile/pcs',
+      'en.wikipedia.org/api/rest_v1/data/css/mobile/site',
+    ]
+    const offlineResourcesJSList = ['meta.wikimedia.org/api/rest_v1/data/javascript/mobile/pcs']
+
+    offlineResourcesCSSList.forEach((cssUrl) => {
+      const linkEl = doc.createElement('link') as DominoElement
+      Object.assign(linkEl, { rel: 'stylesheet', href: `${protocol}${cssUrl}` })
+      doc.head.appendChild(linkEl)
+    })
+
+    offlineResourcesJSList.forEach((jsUrl) => {
+      const scriptEl = doc.createElement('script') as DominoElement
+      scriptEl.setAttribute('src', `${protocol}${jsUrl}`)
+      doc.head.appendChild(scriptEl)
+    })
+
+    return doc.documentElement.outerHTML
   }
 }
