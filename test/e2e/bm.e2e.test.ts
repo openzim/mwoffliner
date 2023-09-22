a98d7809da94211cd05cbe8322748298bf39689b
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2023-09-22 09:26:50 +0300

    Connect VE to bm.e2e.test.ts

--- test/e2e/bm.e2e.test.ts
+++ test/e2e/bm.e2e.test.ts
@@ -10,84 +10,81 @@ jest.setTimeout(200000)
 
 describe('bm', () => {
   for (const renderer of RENDERERS_LIST) {
-    // Test only render API that is supported by the wiki
-    if (renderer === 'WikimediaDesktop') {
-      const now = new Date()
-      const testId = `mwo-test-${+now}`
+    const now = new Date()
+    const testId = `mwo-test-${+now}`
 
-      const parameters = {
-        mwUrl: 'https://bm.wikipedia.org',
-        adminEmail: 'test@kiwix.org',
-        outputDirectory: testId,
-        redis: process.env.REDIS,
-        format: ['nopic'],
+    const parameters = {
+      mwUrl: 'https://bm.wikipedia.org',
+      adminEmail: 'test@kiwix.org',
+      outputDirectory: testId,
+      redis: process.env.REDIS,
+      format: ['nopic'],
+    }
+
+    const renderParameters = { ...parameters, forceRender: renderer }
+
+    test(`Simple articleList (${renderer} renderer)`, async () => {
+      await execa('redis-cli flushall', { shell: true })
+
+      const outFiles = await mwoffliner.execute(renderParameters)
+
+      // Created 1 output
+      expect(outFiles).toHaveLength(1)
+
+      for (const dump of outFiles) {
+        if (dump.nopic) {
+          // nopic has enough files
+          expect(dump.status.files.success).toBeGreaterThan(14)
+          // nopic has enough redirects
+          expect(dump.status.redirects.written).toBeGreaterThan(170)
+          // nopic has enough articles
+          expect(dump.status.articles.success).toBeGreaterThan(700)
+        }
       }
 
-      const renderParameters = { ...parameters, forceRender: renderer }
+      if (await zimcheckAvailable()) {
+        await expect(zimcheck(outFiles[0].outFile)).resolves.not.toThrowError()
+      } else {
+        console.log('Zimcheck not installed, skipping test')
+      }
 
-      test('Simple articleList', async () => {
-        await execa('redis-cli flushall', { shell: true })
+      if (await zimdumpAvailable()) {
+        const discussionArticlesStr = await zimdump(`list --ns A/Discussion ${outFiles[0].outFile}`)
+        // Articles with "Discussion" namespace should be only with option addNamespaces: 1
+        expect(discussionArticlesStr.length).toBe(0)
+      } else {
+        console.log('Zimdump not installed, skipping test')
+      }
 
-        const outFiles = await mwoffliner.execute(renderParameters)
+      // TODO: clear test dir
+      rimraf.sync(`./${testId}`)
 
-        // Created 1 output
-        expect(outFiles).toHaveLength(1)
+      const redisScan = await execa('redis-cli --scan', { shell: true })
+      // Redis has been cleared
+      expect(redisScan.stdout).toEqual('')
+    })
 
-        for (const dump of outFiles) {
-          if (dump.nopic) {
-            // nopic has enough files
-            expect(dump.status.files.success).toBeGreaterThan(14)
-            // nopic has enough redirects
-            expect(dump.status.redirects.written).toBeGreaterThan(170)
-            // nopic has enough articles
-            expect(dump.status.articles.success).toBeGreaterThan(700)
-          }
-        }
+    test(`Articles with "Discussion" namespace  (${renderer} renderer)`, async () => {
+      await execa('redis-cli flushall', { shell: true })
 
-        if (await zimcheckAvailable()) {
-          await expect(zimcheck(outFiles[0].outFile)).resolves.not.toThrowError()
-        } else {
-          console.log('Zimcheck not installed, skipping test')
-        }
+      const outFiles = await mwoffliner.execute({ ...renderParameters, addNamespaces: 1 })
+      // Created 1 output
+      expect(outFiles).toHaveLength(1)
 
-        if (await zimdumpAvailable()) {
-          const discussionArticlesStr = await zimdump(`list --ns A/Discussion ${outFiles[0].outFile}`)
-          // Articles with "Discussion" namespace should be only with option addNamespaces: 1
-          expect(discussionArticlesStr.length).toBe(0)
-        } else {
-          console.log('Zimdump not installed, skipping test')
-        }
+      if (await zimdumpAvailable()) {
+        const discussionArticlesStr = await zimdump(`list --ns A/Discussion ${outFiles[0].outFile}`)
+        const discussionArticlesList = discussionArticlesStr.match(/Discussion:/g)
+        expect(discussionArticlesList.length).toBeGreaterThan(30)
+      } else {
+        console.log('Zimdump not installed, skipping test')
+      }
 
-        // TODO: clear test dir
-        rimraf.sync(`./${testId}`)
+      // TODO: clear test dir
+      rimraf.sync(`./${testId}`)
 
-        const redisScan = await execa('redis-cli --scan', { shell: true })
-        // Redis has been cleared
-        expect(redisScan.stdout).toEqual('')
-      })
-
-      test('Articles with "Discussion" namespace', async () => {
-        await execa('redis-cli flushall', { shell: true })
-
-        const outFiles = await mwoffliner.execute({ ...renderParameters, addNamespaces: 1 })
-        // Created 1 output
-        expect(outFiles).toHaveLength(1)
-
-        if (await zimdumpAvailable()) {
-          const discussionArticlesStr = await zimdump(`list --ns A/Discussion ${outFiles[0].outFile}`)
-          const discussionArticlesList = discussionArticlesStr.match(/Discussion:/g)
-          expect(discussionArticlesList.length).toBeGreaterThan(30)
-        } else {
-          console.log('Zimdump not installed, skipping test')
-        }
-
-        // TODO: clear test dir
-        rimraf.sync(`./${testId}`)
-
-        const redisScan = await execa('redis-cli --scan', { shell: true })
-        // Redis has been cleared
-        expect(redisScan.stdout).toEqual('')
-      })
-    }
+      const redisScan = await execa('redis-cli --scan', { shell: true })
+      // Redis has been cleared
+      expect(redisScan.stdout).toEqual('')
+    })
   }
 })
