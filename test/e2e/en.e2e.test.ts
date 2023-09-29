956fd7ad2c75f569d37253e4c8c2f56e02926ec8
Vadim Kovalenko <vadim.kovalenko@speedandfunction.com>
2023-09-29 14:46:20 +0300

    Update e2e template

--- test/e2e/en.e2e.test.ts
+++ test/e2e/en.e2e.test.ts
@@ -1,10 +1,51 @@
-import testWithAllRenders from '../testAllRendersTemplate.js'
+import { testAllRenders } from '../testAllRenders.js'
+import domino from 'domino'
+import { zimdump } from '../util.js'
+import 'dotenv/config.js'
+import { jest } from '@jest/globals'
+import rimraf from 'rimraf'
 
-describe('e2e test en.wikipedia.org', () => {
-  it('Run all renders test', async () => {
+jest.setTimeout(60000)
+
+// Check the integrity of img elements between zim file and article html taken from it
+const verifyImgElements = (imgFilesArr, imgElements) => {
+  for (const img of imgElements) {
+    for (const imgFile of imgFilesArr) {
+      if (img.getAttribute('src').includes(imgFile)) {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+// TODO: Jest requires that tests within a describe block be defined synchronously, no async () functions in callback
+describe('e2e test for en.wikipedia.org', () => {
+  test('Test article structure', async () => {
     const mwUrl = 'https://en.wikipedia.org'
     const articleList = 'User:Kelson/MWoffliner_CI_reference'
     const format = ''
-    await testWithAllRenders(mwUrl, articleList, format)
+    await testAllRenders(mwUrl, articleList, format, async (outFiles) => {
+      /*
+        TODO:
+        We have to call expect() here in a callback but without test('', () => {...})
+        Otherwise, receive Error: Tests cannot be nested. Test "some test" cannot run because it is nested within "Test article structure".
+        Uncomment code below to see the issue
+      */
+      /*
+      test('some test', () => {
+        expect(1).toBe(1)
+      })
+      */
+      const articleFromDump = await zimdump(`show --url A/${articleList} ${outFiles[0].outFile}`)
+      const articleDoc = domino.createDocument(articleFromDump)
+      expect(articleDoc.querySelector('h1.article-header')).toBeTruthy()
+      const mediaFiles = await zimdump(`list --ns I ${outFiles[0].outFile}`)
+      const mediaFilesArr = mediaFiles.split('\n')
+      const imgFilesArr = mediaFilesArr.filter((elem) => elem.endsWith('pdf') || elem.endsWith('png') || elem.endsWith('jpg'))
+      const imgElements = Array.from(articleDoc.querySelectorAll('img'))
+      expect(verifyImgElements(imgFilesArr, imgElements)).toBe(true)
+      rimraf.sync(`./${outFiles[0].testId}`)
+    })
   })
 })
---
+++ test/testAllRenders.ts
@@ -0,0 +1,53 @@
+import * as mwoffliner from '../src/mwoffliner.lib.js'
+import { execa } from 'execa'
+import { RENDERERS_LIST } from '../src/util/const.js'
+import { zimcheckAvailable, zimdumpAvailable } from './util.js'
+
+/*
+  This is the template for e2e tests of different wikis
+  1. Verify zimcheck and zimdump availability
+  2. Gets output file and checks its integrity
+  3. Returns output file per renderer in the callback function
+*/
+
+async function getOutFiles(renderName: string, testId: string, articleList: string, mwUrl: string, format?: string | string[]): Promise<any> {
+  const parameters = {
+    mwUrl,
+    adminEmail: 'test@kiwix.org',
+    outputDirectory: testId,
+    redis: process.env.REDIS,
+    articleList,
+    forceRender: renderName,
+    format,
+  }
+
+  await execa('redis-cli flushall', { shell: true })
+  const outFiles = await mwoffliner.execute(parameters)
+
+  return outFiles
+}
+
+export async function testAllRenders(mwUrl: string, articleList: string, format: string | string[], callback) {
+  const zimcheckIsAvailable = await zimcheckAvailable()
+  const zimdumpIsAvailable = await zimdumpAvailable()
+
+  if (!zimcheckIsAvailable || !zimdumpIsAvailable) {
+    const missingTool = !zimcheckIsAvailable ? 'Zimcheck' : 'Zimdump'
+    console.log(`${missingTool} not installed, skipping test`)
+    return
+  }
+
+  for (const renderer of RENDERERS_LIST) {
+    const now = new Date()
+    const testId = `mwo-test-${+now}`
+    const outFiles = await getOutFiles(renderer, testId, articleList, mwUrl, format)
+    outFiles[0].testId = testId
+    outFiles[0].renderer = renderer
+    /*
+    TODO: render name should be passed to the test name somehow.
+    But since jest test methods are not available inside the callback, this is impossible given this pattern
+    */
+    console.log('test renderer:', renderer)
+    await callback(outFiles)
+  }
+}
