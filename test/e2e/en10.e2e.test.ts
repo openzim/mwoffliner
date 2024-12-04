9b7d347c9a90904ed82544fd34eb97e9d81361fd
Travis Briggs <audiodude@gmail.com>
2024-12-03 20:00:13 -0800

    Use temporary article list from WP1 for e2e test

--- test/e2e/en10.e2e.test.ts
+++ test/e2e/en10.e2e.test.ts
@@ -8,7 +8,7 @@ jest.setTimeout(200000)
 const parameters = {
   mwUrl: 'https://en.wikipedia.org',
   adminEmail: 'test@kiwix.org',
-  articleList: 'https://download.openzim.org/wp1/enwiki/tops/10.tsv',
+  articleList: 'https://api.wp1.openzim.org/v1/builders/28a933d2-1903-417d-939e-3b6e8ee7a126/selection/latest.tsv',
   redis: process.env.REDIS,
   // format: ['nopic', 'novid', 'nopdf', 'nodet'],
   format: ['nopic', 'nopdf'],
@@ -24,10 +24,10 @@ await testAllRenders(parameters, async (outFiles) => {
         if (dump.nopic) {
           // nopic has enough files (this is just an estimate and can change
           // with time, as new Mediwiki versions are released).
-          expect(dump.status.files.success).toBeGreaterThan(13)
-          expect(dump.status.files.success).toBeLessThan(22)
+          expect(dump.status.files.success).toBeGreaterThan(50)
+          expect(dump.status.files.success).toBeLessThan(60)
           // nopic has enough redirects
-          expect(dump.status.redirects.written).toBeGreaterThan(480)
+          expect(dump.status.redirects.written).toBeGreaterThan(500)
           // nopic has 10 articles
           expect(dump.status.articles.success).toEqual(10)
           // No article and files error
@@ -35,23 +35,23 @@ await testAllRenders(parameters, async (outFiles) => {
           expect(dump.status.files.fail).toEqual(0)
         } else if (dump.novid) {
           // novid has enough files
-          expect(dump.status.files.success).toBeGreaterThan(420)
+          expect(dump.status.files.success).toBeGreaterThan(314)
           // novid has enough redirects
-          expect(dump.status.redirects.written).toBeGreaterThan(480)
+          expect(dump.status.redirects.written).toBeGreaterThan(314)
           // novid has 10 articles
           expect(dump.status.articles.success).toEqual(10)
         } else if (dump.nopdf) {
           // nopdf has enough files
-          expect(dump.status.files.success).toBeGreaterThan(450)
+          expect(dump.status.files.success).toBeGreaterThan(340)
           // nopdf has enough redirects
-          expect(dump.status.redirects.written).toBeGreaterThan(480)
+          expect(dump.status.redirects.written).toBeGreaterThan(314)
           // nopdf has 10 articles
           expect(dump.status.articles.success).toEqual(10)
         } else if (dump.nodet) {
           // nodet has enough files
           expect(dump.status.files.success).toBeGreaterThan(50)
           // nodet has enough redirects
-          expect(dump.status.redirects.written).toBeGreaterThan(480)
+          expect(dump.status.redirects.written).toBeGreaterThan(314)
           // nodet has 10 articles
           expect(dump.status.articles.success).toEqual(10)
         }
