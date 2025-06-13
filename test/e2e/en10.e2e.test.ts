b86278c6441420a85c910435e26e549799b78ac9
benoit74 <benoit74@users.noreply.github.com>
2025-06-13 13:36:43 +0000

    Return to original top 10 list + allow more slack on number of files
    Since the top 10 is constantly evolving, so does the number of files.

--- test/e2e/en10.e2e.test.ts
+++ test/e2e/en10.e2e.test.ts
@@ -8,7 +8,7 @@ jest.setTimeout(200000)
 const parameters = {
   mwUrl: 'https://en.wikipedia.org',
   adminEmail: 'test@kiwix.org',
-  articleList: 'https://api.wp1.openzim.org/v1/builders/28a933d2-1903-417d-939e-3b6e8ee7a126/selection/latest.tsv',
+  articleList: 'https://download.openzim.org/wp1/enwiki/tops/10.tsv',
   redis: process.env.REDIS,
   // format: ['nopic', 'novid', 'nopdf', 'nodet'],
   format: ['nopic', 'nopdf'],
@@ -24,8 +24,8 @@ await testAllRenders('en10-wikipedia', parameters, async (outFiles) => {
         if (dump.nopic) {
           // nopic has enough files (this is just an estimate and can change
           // with time, as new Mediwiki versions are released).
-          expect(dump.status.files.success).toBeGreaterThanOrEqual(outFiles[0].renderer == 'WikimediaMobile' ? 42 : 47)
-          expect(dump.status.files.success).toBeLessThan(59)
+          expect(dump.status.files.success).toBeGreaterThan(2)
+          expect(dump.status.files.success).toBeLessThan(60)
           // nopic has enough redirects
           expect(dump.status.redirects.written).toBeGreaterThan(500)
           // nopic has 10 articles
