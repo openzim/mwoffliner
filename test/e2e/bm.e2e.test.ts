00702f6bb7f2a87300d310e6b2698585f00a77fd
benoit74 <benoit74@users.noreply.github.com>
2025-07-04 16:40:19 +0000

    Create an HTML redirect instead of a libzim one when redirecting to a section

--- test/e2e/bm.e2e.test.ts
+++ test/e2e/bm.e2e.test.ts
@@ -38,6 +38,38 @@ await testAllRenders('bm-wikipedia', parameters, async (outFiles) => {
     }
   })
 
+  test(`test redirect without fragment ${outFiles[0]?.renderer} renderer`, async () => {
+    // "Bamako" should be a redirect to "Bamakɔ"
+    const redirectInfo = await zimdump(`list --details --url Bamako ${outFiles[0].outFile}`)
+
+    expect(redirectInfo).toMatch(/path:\s*Bamako/)
+    expect(redirectInfo).toMatch(/title:\s*Bamako/)
+    expect(redirectInfo).toMatch(/type:\s*redirect/)
+    const redirectIndexMatch = redirectInfo.match(/redirect index:\s*(\d+)/)
+    expect(redirectIndexMatch).not.toBe(null)
+    const redirectIndex = redirectIndexMatch ? parseInt(redirectIndexMatch[1], 10) : null
+    expect(redirectIndex).not.toBeNull()
+
+    const redirectTargetInfo = await zimdump(`list --details --idx ${redirectIndex} ${outFiles[0].outFile}`)
+    expect(redirectTargetInfo).toMatch(/path:\s*Bamakɔ/)
+    expect(redirectTargetInfo).toMatch(/title:\s*Bamakɔ/)
+    expect(redirectTargetInfo).toMatch(/type:\s*item/)
+  })
+
+  test(`test redirect with fragment ${outFiles[0]?.renderer} renderer`, async () => {
+    // "Bamakɔ_Monumentsa" should be an HTML item which will redirect to "Bamakɔ#Monumentsa_ni_yɔrɔ"
+    // through http-equiv="refresh"
+    const redirectInfo = await zimdump(`list --details --url Bamakɔ_Monumentsa ${outFiles[0].outFile}`)
+
+    expect(redirectInfo).toMatch(/path:\s*Bamakɔ_Monumentsa/)
+    expect(redirectInfo).toMatch(/title:\s*Bamakɔ Monumentsa/)
+    expect(redirectInfo).toMatch(/type:\s*item/)
+
+    const redirectFromDump = await zimdump(`show --url Bamakɔ_Monumentsa ${outFiles[0].outFile}`)
+    expect(redirectFromDump).toContain('<title>Bamakɔ Monumentsa</title>')
+    expect(redirectFromDump).toContain('<meta http-equiv="refresh" content="0;URL=\'./Bamakɔ#Monumentsa_ni_yɔrɔ\'" />')
+  })
+
   afterAll(() => {
     if (!process.env.KEEP_ZIMS) {
       rimraf.sync(`./${outFiles[0].testId}`)
