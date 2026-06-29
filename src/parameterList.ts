4f0d05369ad8670b4616677b53c0d3c28115c84a
benoit74 <benoit74@users.noreply.github.com>
2026-06-29 20:13:34 +0000

    Reintroduce support of --customFlavour option

--- src/parameterList.ts
+++ src/parameterList.ts
@@ -48,6 +48,7 @@ export const parameterDescriptions = {
   customCss: 'Comma-separated list of CSS URLs to inject into all rendered pages',
   userAgent: 'Custom User-Agent header for all HTTP requests. Defaults to "MWOffliner/<version> (<adminEmail>)"',
   stableRevision: 'Prefer stable pages revisions when available, based on FlaggedRevs extension (which needs to be active on the wiki).',
+  customFlavour: 'A custom processor that can filter and process articles (see extensions/*.js)',
 }
 
 // TODO: Add an interface based on the object above
